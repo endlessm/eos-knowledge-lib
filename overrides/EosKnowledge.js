c554b9bfbcd9939f5e03bb7505f2c55ef74f8743
Philip Chimento <philip@endlessm.com>
2014-04-08 15:42:01 -0700

    History model
    This represents a list of pages that have been navigated to during a
    given run of the program. Just like a browser, they keep track of where
    you have visited so far; if you navigate back, then you can also
    navigate forwards again. However, trees are not possible - if you
    navigate back then visit a new page, your forward history is lost.
    
    [endlessm/eos-sdk#829]

--- overrides/EosKnowledge.js
+++ overrides/EosKnowledge.js
@@ -23,6 +23,34 @@ function _init() {
     // here we force the C lib to be initialized along with it its gresource
     this.hello_c;
 
+    // More hackzors. Work around bug where GList-type properties aren't
+    // converted properly into JS values.
+    // https://bugzilla.gnome.org/show_bug.cgi?id=727787
+    Object.defineProperties(EosKnowledge.HistoryModel.prototype, {
+        'back-list': {
+            get: EosKnowledge.HistoryModel.prototype.get_back_list
+        },
+        'forward-list': {
+            get: EosKnowledge.HistoryModel.prototype.get_forward_list
+        },
+        // Once again with underscores, because that's the most used name to
+        // access properties with
+        'back_list': {
+            get: EosKnowledge.HistoryModel.prototype.get_back_list
+        },
+        'forward_list': {
+            get: EosKnowledge.HistoryModel.prototype.get_forward_list
+        },
+        // Once again in camelCaps, because GJS supports that notation too for
+        // properties
+        'backList': {
+            get: EosKnowledge.HistoryModel.prototype.get_back_list
+        },
+        'forwardList': {
+            get: EosKnowledge.HistoryModel.prototype.get_forward_list
+        }
+    });
+
     let modulesToImport = [ArticleCard, Card, LessonCard, ListCard];
     modulesToImport.forEach(function (module) {
         // Inject the EosKnowledge module into the module being imported, to
