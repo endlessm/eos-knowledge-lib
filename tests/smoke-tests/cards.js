6fb734b079d2916e1d6bb65e8e44d7dd7d2b47ea
Philip Chimento <philip@endlessm.com>
2014-04-09 14:40:26 -0700

    Refactor cards smoke test
    Since this branch will be adding more cards, refactor smoke test to
    make the code slightly less repetitive.
    
    [endlessm/eos-sdk#833]

--- tests/smoke-tests/cards.js
+++ tests/smoke-tests/cards.js
@@ -16,50 +16,40 @@ const TestApplication = new Lang.Class ({
         this.parent();
 
         let provider = new Gtk.CssProvider();
-        let css_file = Gio.File.new_for_uri('resource:///com/endlessm/knowledge/endless_knowledge.css')
+        let css_file = Gio.File.new_for_uri('resource:///com/endlessm/knowledge/endless_knowledge.css');
         provider.load_from_file(css_file);
         Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),
                                                  provider,
                                                  Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
 
-        let card1 = new EosKnowledge.Card({
-            title: 'Subtitled Card',
-            subtitle: 'This is the Subtitle',
-            expand: true,
-            valign: Gtk.Align.CENTER,
-            halign: Gtk.Align.CENTER
-        });
-        card1.connect('clicked', function () {
-            print('Card 1 clicked');
-        });
-        let card2 = new EosKnowledge.Card({
-            title: 'Picture Card',
-            thumbnail_uri: TESTDIR + '/test-content/pig1.jpg',
-            expand: true,
-            valign: Gtk.Align.CENTER,
-            halign: Gtk.Align.CENTER
-        });
-        card2.connect('clicked', function () {
-            print('Card 2 clicked');
-        });
-        let card3 = new EosKnowledge.Card({
-            title: 'Everything card',
-            subtitle: 'This card has everything',
-            thumbnail_uri: TESTDIR + '/test-content/pig2.jpg',
-            expand: true,
-            valign: Gtk.Align.CENTER,
-            halign: Gtk.Align.CENTER
-        });
-        card3.connect('clicked', function () {
-            print('Card 3 clicked');
-        });
-
+        let cards = [
+            new EosKnowledge.Card({
+                title: 'Subtitled Card',
+                subtitle: 'This is the Subtitle',
+            }),
+            new EosKnowledge.Card({
+                title: 'Picture Card',
+                thumbnail_uri: TESTDIR + '/test-content/pig1.jpg',
+            }),
+            new EosKnowledge.Card({
+                title: 'Everything card',
+                subtitle: 'This card has everything',
+                thumbnail_uri: TESTDIR + '/test-content/pig2.jpg',
+            })
+        ];
         let grid = new Gtk.Grid({
             orientation: Gtk.Orientation.HORIZONTAL
         });
-        grid.add(card1);
-        grid.add(card2);
-        grid.add(card3);
+
+        cards.forEach(function (card, index) {
+            card.expand = true;
+            card.valign = Gtk.Align.CENTER;
+            card.halign = Gtk.Align.CENTER;
+            card.connect('clicked', function () {
+                print('Card', index + 1, 'clicked');
+            });
+            grid.add(card);
+        });
 
         let window = new Endless.Window({
             application: this
