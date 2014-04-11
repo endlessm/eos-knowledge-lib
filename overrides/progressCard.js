4cc36f08c77afd809cc65ae9abf020187cb7f638
Philip Chimento <philip@endlessm.com>
2014-04-10 17:41:16 -0700

    Progress card
    This Card subclass represents a group of cards in a sequence that can
    be completed or "checked off". Depending on how the display is
    implemented, 5 total-items and 4 completed-items could be displayed as
    "4 out of 5 finished" or "80% done" or something entirely graphical.
    The current manner of displaying them is a placeholder, pending design
    specs.
    
    [endlessm/eos-sdk#836]

---
+++ overrides/progressCard.js
@@ -0,0 +1,118 @@
+// Copyright 2014 Endless Mobile, Inc.
+
+const EosKnowledge = imports.gi.EosKnowledge;
+const Format = imports.format;
+const Gettext = imports.gettext;
+const GLib = imports.gi.GLib;
+const GObject = imports.gi.GObject;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+
+const ListCard = imports.listCard;
+const Config = imports.config;
+
+String.prototype.format = Format.format;
+let _ = Gettext.dgettext.bind(null, Config.GETTEXT_PACKAGE);
+GObject.ParamFlags.READWRITE = GObject.ParamFlags.READABLE | GObject.ParamFlags.WRITABLE;
+
+/**
+ * Class: ProgressCard
+ *
+ * This card represents a group of cards in a sequence that can be completed or
+ * "checked off."
+ * It has two properties, <total-items> and <completed-items>, which can be used
+ * to figure the percentage done.
+ *
+ * Extends:
+ *   <ListCard>
+ */
+const ProgressCard = new Lang.Class({
+    Name: 'ProgressCard',
+    GTypeName: 'EknProgressCard',
+    Extends: ListCard.ListCard,
+    Properties: {
+        /**
+         * Property: total-items
+         *
+         * The number of completable items represented by this card.
+         * If set to 0, then the number will not be displayed.
+         */
+        'total-items': GObject.ParamSpec.uint('total-items', 'Total items',
+            'Number of completable items in total',
+            GObject.ParamFlags.READWRITE,
+            0, GLib.MAXUINT32, 0),
+        /**
+         * Property: completed-items
+         *
+         * The number of completed items out of <total-items>.
+         */
+        'completed-items': GObject.ParamSpec.uint('completed-items',
+            'Completed items', 'Number of completed items',
+            GObject.ParamFlags.READWRITE,
+            0, GLib.MAXUINT32, 0)
+    },
+
+    _init: function (props) {
+        this._total = 0;
+        this._complete = 0;
+
+        this._progress = new Gtk.ProgressBar({
+            halign: Gtk.Align.CENTER,
+            valign: Gtk.Align.CENTER,
+            show_text: true,
+            no_show_all: true
+        });
+        this._progress.get_style_context().add_class('FIXME-private-progress');
+
+        this.parent(props);
+
+        // FIXME Reach into the parent class's defined layout. I guess we can't
+        // do this in production code, but it depends on the designs how we
+        // structure the drawing. This is just a placeholder.
+        let grid = this.get_child();
+        grid.remove(this._frame);
+        grid.attach_next_to(this._progress, this._title_label,
+            Gtk.PositionType.TOP, 1, 1);
+        this.setSensitiveChildren([this._title_label, this._subtitle_label,
+            this._progress]);
+
+        this.show_all();
+    },
+
+    get total_items() {
+        return this._total;
+    },
+
+    set total_items(value) {
+        this._total = value;
+        this._update_bar();
+    },
+
+    get completed_items() {
+        let realComplete = Math.min(this._complete, this._total);
+        return realComplete;
+    },
+
+    set completed_items(value) {
+        this._complete = value;
+        this._update_bar();
+    },
+
+    // Private
+
+    _update_bar: function () {
+        if (this._total > 0 && this.completed_items === this._total)
+            this.get_style_context().add_class(EosKnowledge.STYLE_CLASS_COMPLETE);
+        else
+            this.get_style_context().remove_class(EosKnowledge.STYLE_CLASS_COMPLETE);
+
+        this._progress.text = _("%d of %d done").format(this.completed_items,
+            this._total);
+        if (this._total > 0) {
+            this._progress.fraction = this.completed_items / this._total;
+            this._progress.show();
+        } else {
+            this._progress.hide();
+        }
+    }
+});
