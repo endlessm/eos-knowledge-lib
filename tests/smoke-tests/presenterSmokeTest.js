4b8b53e1fe1fac7cff417fcaed0dc9ba30c043a9
Rory MacQueen <rorymacqueen@gmail.com>
2014-05-06 17:27:12 -0700

    Added tests for presenter
    [endlessm/eos-sdk#1003]

---
+++ tests/smoke-tests/presenterSmokeTest.js
@@ -0,0 +1,41 @@
+const Gtk = imports.gi.Gtk;
+const Gdk = imports.gi.Gdk;
+const Gio = imports.gi.Gio;
+const Endless = imports.gi.Endless;
+const EosKnowledge = imports.gi.EosKnowledge;
+const Lang = imports.lang;
+
+const TEST_APPLICATION_ID = 'com.endlessm.knowledge.presenter';
+const TESTDIR = Endless.getCurrentFileDir() + '/..';
+
+const TestApplication = new Lang.Class ({
+    Name: 'TestApplication',
+    Extends: Endless.Application,
+
+    vfunc_startup: function() {
+
+        this.parent();
+        let provider = new Gtk.CssProvider();
+        let css_file = Gio.File.new_for_uri('resource:///com/endlessm/knowledge/endless_knowledge.css');
+        provider.load_from_file(css_file);
+        Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),
+                                                 provider,
+                                                 Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+        let win = new EosKnowledge.WindowA({
+            application: this
+        });
+        let test_app_filename = TESTDIR + '/test-content/got_app.json';
+        let presenter = new EosKnowledge.Presenter({
+            view: win
+        }, test_app_filename);
+
+        win.show_all();
+    }
+});
+
+let app = new TestApplication({
+    application_id: TEST_APPLICATION_ID,
+    flags: 0
+});
+app.run(ARGV);
