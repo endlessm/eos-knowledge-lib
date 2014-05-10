b9786f6c8850cecebca9201b46e979152dbd916c
Rory MacQueen <rorymacqueen@gmail.com>
2014-05-09 17:56:37 -0700

    Tests for article presenter
    Smoke tests and jasmine unit test
    
    [endlessm/eos-sdk#1005]

---
+++ tests/eosknowledge/testArticlePresenter.js
@@ -0,0 +1,57 @@
+const Endless = imports.gi.Endless;
+const EosKnowledge = imports.gi.EosKnowledge;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const Gtk = imports.gi.Gtk;
+
+Gtk.init(null);
+
+const TESTDIR = Endless.getCurrentFileDir() + '/..';
+const MOCK_ARTICLE_PATH = TESTDIR + '/test-content/cyprus.jsonld';
+
+describe('Article Presenter', function () {
+    let presenter;
+    let view;
+    let mockArticleData;
+    let articleObject
+    let engine;
+
+    beforeEach(function () {
+
+        let file = Gio.file_new_for_path(MOCK_ARTICLE_PATH);
+
+        let [success, data] = file.load_contents(null);
+        mockArticleData = JSON.parse(data);
+
+        articleObject = new EosKnowledge.ArticleObjectModel.new_from_json_ld(mockArticleData);
+
+        view = new EosKnowledge.ArticlePageA()
+
+        engine = new EosKnowledge.Engine();
+
+        presenter = new EosKnowledge.ArticlePresenter({
+            article_view: view,
+            engine: engine
+        });
+        presenter.load_article_from_model(articleObject);
+
+    });
+
+    it('can be constructed', function () {});
+
+    it('can set title and subtitle on view', function () {
+        expect(view.title).toBe(articleObject.title);
+
+    });
+
+    it('can set toc section list', function () {
+        let labels = [];
+        for (let obj of mockArticleData['tableOfContents']) {
+            if (!('hasParent' in obj)) {
+                labels.push(obj['hasLabel']);
+            }
+        }
+        expect(view.toc.section_list).toEqual(labels);
+    });
+
+});
---
+++ tests/smoke-tests/articlePresenterSmokeTest.js
@@ -0,0 +1,55 @@
+const Endless = imports.gi.Endless;
+const EosKnowledge = imports.gi.EosKnowledge;
+const Gdk = imports.gi.Gdk;
+const Gio = imports.gi.Gio;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+
+const TEST_APPLICATION_ID = 'com.endlessm.knowledge.article_presenter';
+const TESTDIR = Endless.getCurrentFileDir() + '/..';
+
+const MOCK_ARTICLE_PATH = Endless.getCurrentFileDir() + '/../test-content/cyprus.jsonld';
+
+const TestApplication = new Lang.Class ({
+    Name: 'TestApplication',
+    Extends: Endless.Application,
+
+    vfunc_startup: function() {
+
+        this.parent();
+        let provider = new Gtk.CssProvider();
+        let css_file = Gio.File.new_for_uri('resource:///com/endlessm/knowledge/endless_knowledge.css');
+        provider.load_from_file(css_file);
+        Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),
+                                                 provider,
+                                                 Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+        let file = Gio.file_new_for_path(MOCK_ARTICLE_PATH);
+        let [success, data] = file.load_contents(null);
+        let mockArticleData = JSON.parse(data);
+
+        let articleObject = new EosKnowledge.ArticleObjectModel.new_from_json_ld(mockArticleData);
+
+        let view = new EosKnowledge.ArticlePageA()
+        let window = new Endless.Window({
+            application: this
+        });
+        window.get_page_manager().add(view);
+
+        let engine = new EosKnowledge.Engine();
+
+        let presenter = new EosKnowledge.ArticlePresenter({
+            article_view: view,
+            engine: engine
+        });
+        presenter.load_article_from_model(articleObject);
+        window.show_all();
+
+    }
+});
+
+let app = new TestApplication({
+    application_id: TEST_APPLICATION_ID,
+    flags: 0
+});
+app.run(ARGV);
