5e25a618c0dc2c6552bf74697b1e3db0aa8293b0
Matt Watson <mattdangerw@gmail.com>
2014-06-12 15:41:01 -0700

    Improved theming for section page
    Margins, padding and fonts more closely match specs. Section titles
    are all left aligned.
    
    [endlessm/eos-sdk#1309]

--- overrides/sectionPageA.js
+++ overrides/sectionPageA.js
@@ -46,7 +46,8 @@ const SectionPageA = new Lang.Class({
             expand: true,
             valign: Gtk.Align.START,
             row_spacing: 20,
-            margin: 40
+            margin_left: 100,
+            margin_right: 100
         });
 
         this._title_label = new Gtk.Label();
@@ -57,6 +58,13 @@ const SectionPageA = new Lang.Class({
         this.parent(props);
 
         this._content_grid.add(this._title_label);
+
+        // We need the segment titles of all be right aligned with each other.
+        // This gets tricky as they aren't all in the same container, so we
+        // will keep them in a size group.
+        this._right_column_size_group = new Gtk.SizeGroup({
+            mode: Gtk.SizeGroupMode.HORIZONTAL
+        });
 
         this.get_style_context().add_class(EosKnowledge.STYLE_CLASS_SECTION_PAGE);
         this._title_label.get_style_context().add_class(EosKnowledge.STYLE_CLASS_SECTION_PAGE_TITLE);
@@ -83,7 +91,8 @@ const SectionPageA = new Lang.Class({
 
         if (this._segments !== null) {
             for (let segment of this._segments) {
-               this._content_grid.remove(segment);
+                this._right_column_size_group.remove_widget(segment.title_label);
+                this._content_grid.remove(segment);
             }
         }
 
@@ -96,6 +105,7 @@ const SectionPageA = new Lang.Class({
                 let segment = new CardsSegment({
                     title: segment_title
                 });
+                this._right_column_size_group.add_widget(segment.title_label);
 
                 segment.cards = cards;
                 this._segments.push(segment);
@@ -130,13 +140,13 @@ const CardsSegment = new Lang.Class({
         props.row_spacing = 20;
         props.expand = true;
 
-        this._title_label = new Gtk.Label({
-            halign: Gtk.Align.START,
+        this.title_label = new Gtk.Label({
+            xalign: 1,
             valign: Gtk.Align.START,
             wrap: true,
+            justify: Gtk.Justification.RIGHT,
             max_width_chars: 8 // TODO: change this after clarifying with design how resizing should work
         });
-
         let separator = new Gtk.Separator({
             expand: true,
             halign: Gtk.Align.FILL
@@ -152,10 +162,10 @@ const CardsSegment = new Lang.Class({
         this.parent(props);
 
         this.attach(separator, 0, 0, 2, 1);
-        this.attach(this._title_label, 0, 1, 1, 1);
+        this.attach(this.title_label, 0, 1, 1, 1);
         this.attach(this._flow_box, 1, 1, 1, 1);
 
-        this._title_label.get_style_context().add_class(EosKnowledge.STYLE_CLASS_SECTION_PAGE_SEGMENT_TITLE);
+        this.title_label.get_style_context().add_class(EosKnowledge.STYLE_CLASS_SECTION_PAGE_SEGMENT_TITLE);
 
     },
 
@@ -168,7 +178,7 @@ const CardsSegment = new Lang.Class({
     set title (v) {
         if (this._title === v) return;
         this._title = v;
-        this._title_label.label = this._title.toUpperCase();
+        this.title_label.label = this._title.toUpperCase();
         this.notify('title');
     },
 
