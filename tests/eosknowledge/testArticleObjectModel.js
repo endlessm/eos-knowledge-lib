8f78e63eb56461e128751320c4f5eb544b784906
Will Greenberg <will@endlessm.com>
2014-06-26 11:04:56 -0700

    Added tests which verify article objects have resources
    This wasn't tested for before, allowing this bug to slip through
    
    [endlessm/eos-sdk#1482]

--- tests/eosknowledge/testArticleObjectModel.js
+++ tests/eosknowledge/testArticleObjectModel.js
@@ -27,6 +27,18 @@ describe ('Article Object Model', function () {
         it ('should inherit properties set by parent class (ContentObjectModel)', function () {
             expect(articleObject.title).toBeDefined();
             expect(articleObject.synopsis).toBeDefined();
+            expect(articleObject.get_resources()).toBeDefined();
+        });
+
+        it ('should marhsal its resources like a ContentObject', function () {
+            let contentURIs = articleObject.get_resources().map(function (v) {
+                return v.content_uri;
+            });
+            let expectedURIs = mockArticleData.resources.map(function (v) {
+                return v.contentURL;
+            });
+            expect(articleObject.get_resources()[0]).toBeA(EosKnowledge.MediaObjectModel);
+            expect(contentURIs).toEqual(expectedURIs);
         });
 
         it ('should marshal a GtkTreeStore from JSON-LD TreeNodes', function () {
