f0607cd2995e98216e0eb714f7017db2d828e1f3
Fernando Farfan <ffarfan@gmail.com>
2014-06-27 11:32:03 -0600

    Articles can have no TOC
    The ArticlePresenter accepts ArticleObjectModels that have no Table of Contents. In this case, the letf toolbar in the ArticlePageA is hidden.
    
    [endlessm/eos-sdk#1425]

--- tests/smoke-tests/articlePresenterSmokeTest.js
+++ tests/smoke-tests/articlePresenterSmokeTest.js
@@ -10,7 +10,8 @@ EosKnowledge.init();
 const TEST_APPLICATION_ID = 'com.endlessm.knowledge.article_presenter';
 const TESTDIR = Endless.getCurrentFileDir() + '/..';
 
-const MOCK_ARTICLE_PATH = Endless.getCurrentFileDir() + '/../test-content/cyprus.jsonld';
+// const MOCK_ARTICLE_PATH = Endless.getCurrentFileDir() + '/../test-content/cyprus.jsonld';
+const MOCK_ARTICLE_PATH = Endless.getCurrentFileDir() + '/../test-content/cyprus-no-toc.jsonld';
 
 const TestApplication = new Lang.Class ({
     Name: 'TestApplication',
