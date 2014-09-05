f9e2167e18c01048ec2682c9bc9bbbe7bebf696a
goizueta <goizueta@endlessm.com>
2014-09-05 14:03:26 -0700

    Reader "window" that acts as the toplevel view widget.
    The window contains a stack of article pages, nav buttons,
    and a done page. It handles the transitions between pages
    and updates the progress labels of its article pages.
    [endlessm/eos-sdk#2055]

---
+++ tests/smoke-tests/readerWindowSmokeTest.js
@@ -0,0 +1,51 @@
+const Endless = imports.gi.Endless;
+const EosKnowledge = imports.gi.EosKnowledge;
+const Gdk = imports.gi.Gdk;
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+
+const TEST_APPLICATION_ID = 'com.endlessm.knowledge.test.reader-window';
+const TESTDIR = Endless.getCurrentFileDir() + '/..';
+
+const TestApplication = new Lang.Class({
+    Name: 'TestApplication',
+    Extends: Endless.Application,
+
+    vfunc_startup: function () {
+        this.parent();
+
+        let provider = new Gtk.CssProvider();
+        let css_file = Gio.File.new_for_uri('resource:///com/endlessm/knowledge/endless_reader.css');
+        provider.load_from_file(css_file);
+        Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),
+                                                 provider,
+                                                 Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+        let window = new EosKnowledge.Reader.Window({
+            application: this,
+        });
+
+        for (let i = 0; i < 15; i++) {
+            let a = new EosKnowledge.Reader.ArticlePage({ title : 'Example page #' + (i + 1) });
+            window.append_article_page(a);
+        }
+
+        window.show_all();
+        window.nav_buttons.connect('back-clicked', function() {
+            // Decrement current page if it is less than 0.
+            window.current_page = window.current_page > 0 ? window.current_page - 1 : 0;
+        });
+
+        window.nav_buttons.connect('forward-clicked', function() {
+            window.current_page = window.current_page < window.total_pages - 1 ? window.current_page + 1 : window.current_page;
+        });
+    },
+});
+
+let app = new TestApplication({
+    application_id: TEST_APPLICATION_ID,
+    flags: 0,
+});
+app.run(ARGV);
