38bdc2ac9c067a9d2d4699619b976fa3c20f38d4
Matt Watson <mattdangerw@gmail.com>
2014-09-24 17:03:40 -0600

    Always show internal children to pdfview
    We make a new evince view inside of the pdf view every time,
    load_uri is called. We need to make sure to show it when we do
    [endlessm/eos-sdk#2246]

--- overrides/PDFView.js
+++ overrides/PDFView.js
@@ -39,6 +39,7 @@ const PDFView = new Lang.Class({
         });
         let view = new EvinceView.View();
         view.set_model(document_model);
+        view.show_all();
 
         let child = this.get_child();
         if (child !== null)
