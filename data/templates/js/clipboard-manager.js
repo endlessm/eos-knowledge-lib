90ae027424d617bd18ec0c81db7e2f4c592b32aa
Matt Watson <mattdangerw@gmail.com>
2015-01-26 12:37:41 -0800

    Move in all javascript for article templates
    Mostly from knowledge engine, some was already here in knowledge lib
    but injected before
    [endlessm/eos-sdk#2638]

---
+++ data/templates/js/clipboard-manager.js
@@ -0,0 +1,52 @@
+/** clipboard_manager.js
+ *  This utility handles the Copy/Paste behavior for the Knowledge Library.
+ *
+ *  By injecting this code into the articles, the user can select a block of text
+ */
+const FADE_DURATION = 200;
+var webview_focus = true;
+
+$(document).ready(function () {
+    var mouse_is_down = false;
+    // Updates the copy button to appear at the beginning of the selection. If the
+    // mouse is currently down we hide the copy button until the selection
+    // drag is completed.
+    var update_copy_button = function() {
+        var selection = window.getSelection();
+        if (selection.isCollapsed || mouse_is_down || !webview_focus) {
+            $('#copy-button').fadeOut(FADE_DURATION);
+        } else {
+            var range = selection.getRangeAt(0).cloneRange();
+            range.collapse(true);
+            var rect = range.getClientRects()[0];
+
+            var top_position = Math.max(rect.top - $('#copy-button').outerHeight(true), 0) + $(window).scrollTop();
+            var left_position =  Math.max(rect.left - $('#copy-button').outerWidth(true), 0) + $(window).scrollLeft();
+            $('#copy-button').css({
+                top: top_position,
+                left: left_position,
+            });
+            $('#copy-button').fadeIn(FADE_DURATION);
+        }
+    };
+
+    // Update copy button location on any selectionchange or mouseup.
+    $(document).mousedown(function() {
+        mouse_is_down = true;
+    }).mouseup(function() {
+        mouse_is_down = false;
+        update_copy_button();
+    }).on('selectionchange', function() {
+        update_copy_button();
+    });
+
+    $('#copy-button').mousedown(function (event) {
+        // Returning false prevents mousedown signal from propagating up the DOM tree.
+        // This keeps us from losing our selection when clicking on the copy
+        // button.
+        return false;
+    }).click(function () {
+        document.execCommand('Copy');
+        $('#copy-button').fadeOut(FADE_DURATION);
+    });
+});
---
+++ data/templates/js/no-link-remover.js
@@ -0,0 +1,15 @@
+// Ensures that all tags with class .eos-no-link are <span>
+// elements, so that they do not appear clickable, but still
+// retain any attributes they originally had.
+// Borrowed from: http://stackoverflow.com/questions/7093417/using-jquery-to-replace-one-tag-with-another/20493562#20493562
+$('.eos-no-link').replaceWith(function () {
+    var replacement = $('<span>').html(this.innerHTML);
+    for (var i = 0; i < this.attributes.length; i++) {
+        var attr_name = this.attributes[i].name;
+        var attr_value = this.attributes[i].value;
+        if (attr_name !== 'title') {
+            replacement.attr(attr_name, attr_value);
+        }
+    }
+    return replacement;
+});
