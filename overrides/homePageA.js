8388136160e457e5a4ef7b6b380cf79179aeb339
Philip Chimento <philip@endlessm.com>
2015-02-27 14:02:43 -0800

    Port HomePageA to use SpaceContainer
    This removes the custom class and uses a horizontally-oriented
    SpaceContainer with spacing instead.
    
    [endlessm/eos-sdk#2754]

--- overrides/homePageA.js
+++ overrides/homePageA.js
@@ -9,11 +9,14 @@ const Lang = imports.lang;
 
 const Config = imports.config;
 const HomePage = imports.homePage;
+const SpaceContainer = imports.spaceContainer;
 const TabButton = imports.tabButton;
 
 let _ = Gettext.dgettext.bind(null, Config.GETTEXT_PACKAGE);
 
 const BUTTON_TRANSITION_DURATION = 500;
+const MAX_CARDS = 6;
+const CARD_EXTRA_MARGIN = 8;
 
 /**
  * Class: HomePageA
@@ -34,10 +37,12 @@ const HomePageA = new Lang.Class({
     },
 
     _init: function (props) {
-        this._card_container = new HomePageACardContainer({
+        this._card_container = new SpaceContainer.SpaceContainer({
             halign: Gtk.Align.CENTER,
             valign: Gtk.Align.CENTER,
-            expand: true
+            expand: true,
+            orientation: Gtk.Orientation.HORIZONTAL,
+            spacing: CARD_EXTRA_MARGIN,
         });
         this._card_container.get_style_context().add_class(EosKnowledge.STYLE_CLASS_CARD_CONTAINER);
 
@@ -62,7 +67,8 @@ const HomePageA = new Lang.Class({
                 this._button_stack.disconnect(id);
             }.bind(this));
         }.bind(this));
-        this._card_container.connect('notify::all-cards-visible', this._update_button_visibility.bind(this));
+        this._card_container.connect('notify::all-visible',
+            this._update_button_visibility.bind(this));
 
         this._button_stack.add(this._invisible_frame);
         this._button_stack.add(this._all_categories_button);
@@ -85,7 +91,7 @@ const HomePageA = new Lang.Class({
     },
 
     _update_button_visibility: function () {
-        if (!this._card_container.all_cards_visible && !this._animating && this.get_mapped()) {
+        if (!this._card_container.all_visible && !this._animating && this.get_mapped()) {
             this._show_button();
         } else {
             this._hide_button();
@@ -93,7 +99,7 @@ const HomePageA = new Lang.Class({
     },
 
     _hide_button: function (widget) {
-        this._button_stack.transition_type = Gtk.StackTransitionType.SLIDE_DOWN,
+        this._button_stack.transition_type = Gtk.StackTransitionType.SLIDE_DOWN;
         this._button_stack.visible_child = this._invisible_frame;
     },
 
@@ -125,115 +131,15 @@ const HomePageA = new Lang.Class({
     },
 
     pack_cards: function (cards) {
-        this._card_container.remove_cards();
+        this._card_container.get_children().forEach((card) =>
+            this._card_container.remove(card));
         let sorted_cards = cards.sort((a, b) => {
             let sortVal = 0;
             if (a.featured) sortVal--;
             if (b.featured) sortVal++;
             return sortVal;
         });
-        this._card_container.add_cards(sorted_cards);
-    }
-});
-
-// Private class to handle the special card container we need on the home
-// page. Will show as many cards horizontally as there are space for, though
-// always shows at least one card.
-const HomePageACardContainer = new Lang.Class({
-    Name: 'HomePageACardContainer',
-    GTypeName: 'EknHomePageACardContainer',
-    Extends: Endless.CustomContainer,
-
-    Properties: {
-        'all-cards-visible': GObject.ParamSpec.boolean('all-cards-visible',
-            'All cards visible', 'True if all cards are currently being shown.',
-            GObject.ParamFlags.READABLE, true),
-    },
-
-    CARD_EXTRA_MARGIN: 8,
-    MAX_CARDS: 6,
-
-    _init: function (props) {
-        this._cards = [];
-        this._all_cards_visible = true;
-        this.parent(props);
-    },
-
-    get all_cards_visible () {
-        return this._all_cards_visible;
-    },
-
-    add_cards: function (cards) {
-        this._cards = cards;
-        for (let card of cards) {
-            this.add(card);
-        }
-    },
-
-    remove_cards: function () {
-        for (let card of this._cards) {
-            this.remove(card);
-        }
-        this._cards = [];
-    },
-
-    vfunc_size_allocate: function (alloc) {
-        this.parent(alloc);
-        if (this._cards.length === 0)
-            return;
-        let [min, nat] = this._cards_max_preferred_width();
-        let visible_cards =  Math.min(Math.floor(alloc.width / min), this.MAX_CARDS);
-        let total_width = alloc.width;
-        alloc.width  = Math.min(alloc.width / visible_cards, nat);
-        // Always center the cards in the given allocation
-        let extra_width = total_width - alloc.width * visible_cards;
-        alloc.x += extra_width / 2;
-        let all_cards_visible = true;
-        for (let card_index = 0; card_index < this._cards.length; card_index++) {
-            let card = this._cards[card_index];
-            if (card_index < visible_cards) {
-                card.size_allocate(alloc);
-                alloc.x += alloc.width;
-                card.set_child_visible(true);
-            } else {
-                card.set_child_visible(false);
-                all_cards_visible = false;
-            }
-        }
-        if (all_cards_visible !== this._all_cards_visible) {
-            this._all_cards_visible = all_cards_visible;
-            this.notify('all-cards-visible');
-        }
-    },
-
-    _cards_max_preferred_width: function () {
-        let min = 0, nat = 0;
-        for (let card of this._cards) {
-            let [card_min, card_nat] = card.get_preferred_width();
-            min = Math.max(min, card_min);
-            nat = Math.max(nat, card_nat);
-        }
-        if (this._cards.length > 0) {
-            min += this.CARD_EXTRA_MARGIN;
-            nat += this.CARD_EXTRA_MARGIN;
-        }
-        return [min, nat];
-    },
-
-    vfunc_get_preferred_width: function () {
-        let [min, nat] = this._cards_max_preferred_width();
-        // Try to show all cards at natural width, but at worst we show one at
-        // minimal width.
-        return [min, nat * this._cards.length];
-    },
-
-    vfunc_get_preferred_height: function () {
-        let min = 0, nat = 0;
-        for (let card of this._cards) {
-            let [card_min, card_nat] = card.get_preferred_height();
-            min = Math.max(min, card_min);
-            nat = Math.max(nat, card_nat);
-        }
-        return [min, nat];
+        sorted_cards.slice(0, MAX_CARDS).forEach((card) =>
+            this._card_container.add(card));
     }
 });
