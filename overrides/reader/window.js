04e8166725f0682142e6ac1fe457e9a1b3e565f2
Fernando Farfan <ffarfan@gmail.com>
2015-03-12 16:39:46 -0600

    Cleaned up unneeded getters
    Cleaned up some default getters that correspond to read-only properties.
    
    [endlessm/eos-sdk#2565]

--- overrides/reader/window.js
+++ overrides/reader/window.js
@@ -152,29 +152,29 @@ const Window = new Lang.Class({
     _init: function (props) {
         props = props || {};
 
-        this._overview_page = new OverviewPage.OverviewPage();
-        this._done_page = new DonePage.DonePage();
-        this._standalone_page = new StandalonePage.StandalonePage();
-        this._standalone_page.article_page.progress_label.no_show_all = true;
+        this.overview_page = new OverviewPage.OverviewPage();
+        this.done_page = new DonePage.DonePage();
+        this.standalone_page = new StandalonePage.StandalonePage();
+        this.standalone_page.article_page.progress_label.no_show_all = true;
         this.search_results_page = new SearchResultsPage.SearchResultsPage();
-        this._nav_buttons = new NavButtonOverlay.NavButtonOverlay({
+        this.nav_buttons = new NavButtonOverlay.NavButtonOverlay({
             back_image_uri: this._BACK_IMAGE_URI,
             forward_image_uri: this._FORWARD_IMAGE_URI,
             image_size: this._NAV_IMAGE_SIZE,
         });
 
-        this._issue_nav_buttons = new Endless.TopbarNavButton({
+        this.issue_nav_buttons = new Endless.TopbarNavButton({
             no_show_all: true,
         });
         // No need for localization; this is debug only
-        this._issue_nav_buttons.back_button.label = 'Reset';
-        this._issue_nav_buttons.forward_button.label = 'Next week';
+        this.issue_nav_buttons.back_button.label = 'Reset';
+        this.issue_nav_buttons.forward_button.label = 'Next week';
 
-        this._lightbox = new Lightbox.Lightbox();
-        this._lightbox.connect('navigation-previous-clicked', function (lightbox) {
+        this.lightbox = new Lightbox.Lightbox();
+        this.lightbox.connect('navigation-previous-clicked', function (lightbox) {
             this.emit('lightbox-nav-previous-clicked', lightbox);
         }.bind(this));
-        this._lightbox.connect('navigation-next-clicked', function (lightbox) {
+        this.lightbox.connect('navigation-next-clicked', function (lightbox) {
             this.emit('lightbox-nav-next-clicked', lightbox);
         }.bind(this));
 
@@ -193,17 +193,17 @@ const Window = new Lang.Class({
         this._stack = new Gtk.Stack({
             transition_duration: this._STACK_TRANSITION_TIME,
         });
-        this._stack.add(this._overview_page);
-        this._stack.add(this._done_page);
-        this._stack.add(this._standalone_page);
+        this._stack.add(this.overview_page);
+        this._stack.add(this.done_page);
+        this._stack.add(this.standalone_page);
         this._stack.add(this.search_results_page);
-        this._nav_buttons.add(this._stack);
-        this._lightbox.add(this._nav_buttons);
-        this.page_manager.add(this._lightbox, {
-            center_topbar_widget: this._issue_nav_buttons,
+        this.nav_buttons.add(this._stack);
+        this.lightbox.add(this.nav_buttons);
+        this.page_manager.add(this.lightbox, {
+            center_topbar_widget: this.issue_nav_buttons,
         });
-        this._overview_page.show_all();
-        this._stack.set_visible_child(this._overview_page);
+        this.overview_page.show_all();
+        this._stack.set_visible_child(this.overview_page);
     },
 
     _update_progress_labels: function () {
@@ -212,8 +212,8 @@ const Window = new Lang.Class({
             this._article_pages[i].progress_label.current_page = i + 2;
             this._article_pages[i].progress_label.total_pages = this.total_pages;
         }
-        this._done_page.progress_label.current_page = this.total_pages;
-        this._done_page.progress_label.total_pages = this.total_pages;
+        this.done_page.progress_label.current_page = this.total_pages;
+        this.done_page.progress_label.total_pages = this.total_pages;
     },
 
     /*
@@ -258,12 +258,12 @@ const Window = new Lang.Class({
     },
 
     show_standalone_page: function () {
-        this._standalone_page.show();
-        this._standalone_page.article_page.progress_label.hide();
+        this.standalone_page.show();
+        this.standalone_page.article_page.progress_label.hide();
         this._stack.set_transition_type(Gtk.StackTransitionType.SLIDE_UP);
-        this._stack.set_visible_child(this._standalone_page);
-        this._nav_buttons.back_visible = false;
-        this._nav_buttons.forward_visible = false;
+        this._stack.set_visible_child(this.standalone_page);
+        this.nav_buttons.back_visible = false;
+        this.nav_buttons.forward_visible = false;
     },
 
     show_search_results_page: function () {
@@ -273,7 +273,7 @@ const Window = new Lang.Class({
     },
 
     show_article_page: function (index, transition_forward) {
-        this._nav_buttons.accommodate_scrollbar = true;
+        this.nav_buttons.accommodate_scrollbar = true;
         if (transition_forward) {
             this._stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT);
         } else {
@@ -285,40 +285,16 @@ const Window = new Lang.Class({
     },
 
     show_overview_page: function () {
-        this._nav_buttons.accommodate_scrollbar = false;
+        this.nav_buttons.accommodate_scrollbar = false;
         this._stack.set_transition_type(Gtk.StackTransitionType.SLIDE_RIGHT);
-        this._overview_page.show();
-        this._stack.set_visible_child(this._overview_page);
+        this.overview_page.show();
+        this._stack.set_visible_child(this.overview_page);
     },
 
     show_done_page: function () {
         this._stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT);
-        this._done_page.show();
-        this._stack.set_visible_child(this._done_page);
-    },
-
-    get lightbox() {
-        return this._lightbox;
-    },
-
-    get overview_page() {
-        return this._overview_page;
-    },
-
-    get done_page() {
-        return this._done_page;
-    },
-
-    get standalone_page() {
-        return this._standalone_page;
-    },
-
-    get nav_buttons() {
-        return this._nav_buttons;
-    },
-
-    get issue_nav_buttons() {
-        return this._issue_nav_buttons;
+        this.done_page.show();
+        this._stack.set_visible_child(this.done_page);
     },
 
     get total_pages() {
