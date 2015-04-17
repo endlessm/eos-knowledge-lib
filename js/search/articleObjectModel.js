9a39587f059dd464069bc663afaed228c09157ea
Will Greenberg <will@endlessm.com>
2015-04-16 19:20:51 -0700

    Handle legacy ArticleObjectModel formats
    Check the ekn_version to ensure content-type is set for old articles.
    
    Also deprecated the html property, since new bundles won't store that in the
    model itself.
    
    [endlessm/eos-sdk#2917]

--- js/search/articleObjectModel.js
+++ js/search/articleObjectModel.js
@@ -23,7 +23,10 @@ const ArticleObjectModel = new Lang.Class({
     Properties: {
         /**
          * Property: html
-         * Body HTML of the article.
+         * (DEPRECATED) Body HTML of the article.
+         *
+         * No longer used in ekn bundles. Instead, read the compressed HTML at
+         * content_uri in newer bundles.
          */
         'html': GObject.ParamSpec.string('html', 'Article HTML',
             'The HTML of the article, unstyled.',
@@ -165,8 +168,17 @@ ArticleObjectModel._props_from_json_ld = function (json_ld_data, media_path, ekn
     let ParentClass = ArticleObjectModel.__super__;
     let props = ParentClass._props_from_json_ld(json_ld_data, media_path, ekn_version);
 
-    if (json_ld_data.hasOwnProperty('articleBody'))
-        props.html = json_ld_data.articleBody;
+    if (ekn_version === 1) {
+        // This is to support legacy databases, which store their HTML within
+        // the xapian databases themselves (as opposed to using a contentURL to
+        // point to content on disk) and don't store a contentType property
+        if (json_ld_data.hasOwnProperty('articleBody')) {
+            props.html = json_ld_data.articleBody;
+            props.content_type = 'text/html';
+        } else if (props.content_uri) {
+            props.content_type = 'application/pdf';
+        }
+    }
 
     // FIXME: see https://github.com/endlessm/eos-sdk/issues/2520
     // This is a holdover from knowledge engine where we guess the source of
