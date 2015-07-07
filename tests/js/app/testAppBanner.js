3fe291df4b0da06c3fea5f94e18738098fb76f04
Philip Chimento <philip@endlessm.com>
2015-07-07 13:29:20 -0700

    AppBanner:image-uri should get a full URI
    Constructing the URI with string manipulation was not handling relative
    paths, causing the test to fail when run locally with jasmine.
    
    [endlessm/eos-sdk#3351]

--- tests/js/app/testAppBanner.js
+++ tests/js/app/testAppBanner.js
@@ -1,3 +1,4 @@
+const Gio = imports.gi.Gio;
 const Gtk = imports.gi.Gtk;
 
 const Utils = imports.tests.utils;
@@ -11,7 +12,7 @@ const TEST_CONTENT_DIR = Utils.get_test_content_srcdir();
 
 describe('App banner module', function () {
     let app_banner;
-    let pig_uri = 'file://' + TEST_CONTENT_DIR + 'pig1.jpg';
+    let pig_uri = Gio.File.new_for_path(TEST_CONTENT_DIR).get_child('pig1.jpg').get_uri();
 
     beforeEach(function () {
         app_banner = new AppBanner.AppBanner({
