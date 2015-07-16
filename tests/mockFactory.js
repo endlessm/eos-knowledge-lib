a7d22329eaaa4a7d2ac219fca9b98e1431bb1c04
Philip Chimento <philip@endlessm.com>
2015-07-16 16:49:34 -0700

    MockFactory can return created mock instances
    This adds a method to the MockFactory to return all created instances of
    a named mock. This is so that you can assert things that happend to that
    mock instance in your test.
    
    [endlessm/eos-sdk#3382]

--- tests/mockFactory.js
+++ tests/mockFactory.js
@@ -9,15 +9,26 @@ const MockFactory = new Lang.Class({
     _init: function (props={}) {
         this.parent(props);
         this._mock_classes = {};
+        this._created_mocks = {};
     },
 
     create_named_module: function (name, props) {
-        if (this._mock_classes.hasOwnProperty(name))
-            return new this._mock_classes[name](props);
-        return new Gtk.Label();
+        this._created_mocks[name] = this._created_mocks[name] || [];
+        if (this._mock_classes.hasOwnProperty(name)) {
+            let retval = new this._mock_classes[name](props);
+            this._created_mocks[name].push(retval);
+            return retval;
+        }
+        let retval = new Gtk.Label();
+        this._created_mocks[name].push(retval);
+        return retval;
     },
 
     add_named_mock: function (name, klass) {
         this._mock_classes[name] = klass;
+    },
+
+    get_created_named_mocks: function (name) {
+        return this._created_mocks[name] || [];
     },
 });
