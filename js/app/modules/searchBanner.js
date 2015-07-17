4609de017b6f42720e3ea872030874633bacbc13
Fernando Farfan <ffarfan@gmail.com>
2015-07-17 11:23:19 -0600

    Added SearchBanner
    The SearchBanner widget is a specific banner for the Search Results page, and
    it allows us to separate the Search Results Page from the Section Page.
    
    [endlessm/eos-sdk#3372]

---
+++ js/app/modules/searchBanner.js
@@ -0,0 +1,36 @@
+const GObject = imports.gi.GObject;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+
+const Module = imports.app.interfaces.module;
+const Utils = imports.app.utils;
+
+const SearchBanner = new Lang.Class({
+    Name: 'SearchBanner',
+    GTypeName: 'EknSearchBanner',
+    Extends: Gtk.Label,
+    Implements: [ Module.Module ],
+
+    Properties: {
+        'factory': GObject.ParamSpec.override('factory', Module.Module),
+        'query': GObject.ParamSpec.string('query', 'Query', 'Search query',
+            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT, ''),
+    },
+
+    Template: 'resource:///com/endlessm/knowledge/widgets/searchBanner.ui',
+
+    _init: function (props={}) {
+        this.parent(props);
+    },
+
+    get query() {
+        return this._query;
+    },
+
+    set query(value) {
+        if (this._query === value)
+            return;
+        this._query = value;
+        this.label = Utils.page_title_from_query_object(this._query);
+    },
+});
