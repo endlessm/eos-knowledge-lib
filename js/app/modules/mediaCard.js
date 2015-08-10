d5ec1d9f2fff4130d715b810b9704479dce0ad7c
Fernando Farfan <ffarfan@gmail.com>
2015-08-10 11:29:27 -0600

    Show license of media viewed in lightbox
    The lightbox that we use to display media content had always presented the
    attribution information (license, owner), but clicking on the button was doing
    nothing! Now we can open and display the license file for CC and GNU licenses
    when the user presses the button.
    
    Also, when the media displayed in a lightbox is attributed under a license type
    that we include, the attribution label changes color on hover.
    
    [endlessm/eos-sdk#3445]

--- js/app/modules/mediaCard.js
+++ js/app/modules/mediaCard.js
@@ -1,3 +1,5 @@
+const Endless = imports.gi.Endless;
+const Gdk = imports.gi.Gdk;
 const Gio = imports.gi.Gio;
 const GObject = imports.gi.GObject;
 const Gtk = imports.gi.Gtk;
@@ -58,15 +60,27 @@ const MediaCard = new Lang.Class({
         this.set_label_or_hide(this._caption_label,
                                this.model.caption.split('\n').join(' '));
 
+        this.license_uri = '';
         let attributions = [];
-        if (this.model.license)
-            attributions.push(this.model.license);
+        this._attribution_button.sensitive = false;
+        if (this.model.license) {
+            let license_display_name = Endless.get_license_display_name(this.model.license);
+            if (license_display_name) {
+                attributions.push(license_display_name);
+                let license_file = Endless.get_license_file(this.model.license);
+                if (license_file !== null) {
+                    this.license_uri = license_file.get_uri();
+                    this._attribution_button.sensitive = true;
+                }
+            }
+        }
         if (this.model.copyright_holder)
             attributions.push(this.model.copyright_holder);
         this.set_label_or_hide(this._attribution_label, attributions.map((s) => {
             return s.split('\n')[0];
         }).join(' - ').toUpperCase());
         this._attribution_button.visible = this._attribution_label.visible;
+        this._attribution_button.connect('clicked', this._show_license_in_external_viewer.bind(this));
 
         this._separator.visible = this._attribution_label.visible &&
                                   this._caption_label.visible;
@@ -76,5 +90,10 @@ const MediaCard = new Lang.Class({
         let padding = this.get_style_context().get_padding(this.get_state_flags());
         return this._previewer.get_preferred_width().map((v) =>
             Math.max(this._MIN_WIDTH, v) + padding.right + padding.left);
+    },
+
+    _show_license_in_external_viewer: function () {
+        if (this.license_uri !== '')
+            Gtk.show_uri(null, this.license_uri, Gdk.CURRENT_TIME);
     },
 });
