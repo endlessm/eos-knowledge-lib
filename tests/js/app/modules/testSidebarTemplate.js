36219ba5728548d7b72ee58ea77d004dd687bff6
Philip Chimento <philip@endlessm.com>
2015-09-09 17:25:28 -0700

    Make SidebarTemplate responsive
    Restore the behaviour from OverviewPage where a certain amount is taken
    off the margin of its submodules when the page is thinner than a certain
    threshold.
    
    There is a pre-existing defect that remains broken in the new code: no
    adjustment takes place when you unmaximize the window.
    
    [endlessm/eos-sdk#3601]

--- tests/js/app/modules/testSidebarTemplate.js
+++ tests/js/app/modules/testSidebarTemplate.js
@@ -6,20 +6,25 @@ const CssClassMatcher = imports.tests.CssClassMatcher;
 const MockFactory = imports.tests.mockFactory;
 const SidebarTemplate = imports.app.modules.sidebarTemplate;
 const StyleClasses = imports.app.styleClasses;
+const Utils = imports.tests.utils;
 const WidgetDescendantMatcher = imports.tests.WidgetDescendantMatcher;
 
 Gtk.init(null);
 
 describe('Sidebar template', function () {
-    let template, factory;
+    let template, factory, content, sidebar;
 
     beforeEach(function () {
         jasmine.addMatchers(CssClassMatcher.customMatchers);
         jasmine.addMatchers(WidgetDescendantMatcher.customMatchers);
 
         factory = new MockFactory.MockFactory();
-        factory.add_named_mock('mock-sidebar', Gtk.Label);
-        factory.add_named_mock('mock-content', Gtk.Label);
+        factory.add_named_mock('mock-sidebar', Gtk.Label, {}, {
+            margin_end: 200,
+        });
+        factory.add_named_mock('mock-content', Gtk.Label, {}, {
+            margin_start: 200,
+        });
         factory.add_named_mock('sidebar-template', SidebarTemplate.SidebarTemplate,
         {
             'sidebar': 'mock-sidebar',
@@ -30,14 +35,15 @@ describe('Sidebar template', function () {
             factory: factory,
             factory_name: 'sidebar-template',
         });
+
+        sidebar = factory.get_created_named_mocks('mock-sidebar')[0];
+        content = factory.get_created_named_mocks('mock-content')[0];
     });
 
     it('constructs', function () {});
 
     it('packs all its children', function () {
-        let sidebar = factory.get_created_named_mocks('mock-sidebar')[0];
         expect(template).toHaveDescendant(sidebar);
-        let content = factory.get_created_named_mocks('mock-content')[0];
         expect(template).toHaveDescendant(content);
     });
 
@@ -51,5 +57,34 @@ describe('Sidebar template', function () {
 
     it('has the sidebar-frame class on its sidebar frame', function () {
         expect(template).toHaveDescendantWithCssClass(StyleClasses.SIDEBAR);
+    });
+
+    describe('at different sizes', function () {
+        let win;
+
+        beforeEach(function () {
+            win = new Gtk.OffscreenWindow();
+            template.expand = true;
+            win.add(template);
+            win.show_all();
+        });
+
+        afterEach(function () {
+            win.destroy();
+        });
+
+        it('reduces the margins on its submodules when there is less room', function () {
+            win.set_size_request(500, 500);
+            Utils.update_gui();
+            expect(content.margin_start).toBeLessThan(200);
+            expect(sidebar.margin_end).toBeLessThan(200);
+        });
+
+        it('leaves the margins alone when there is enough room', function () {
+            win.set_size_request(1500, 1500);
+            Utils.update_gui();
+            expect(content.margin_start).toBe(200);
+            expect(sidebar.margin_end).toBe(200);
+        });
     });
 });
