d354732aaf5df6f3b50a4bf3b7957f5ea7deabb7
Philip Chimento <philip@endlessm.com>
2015-09-17 17:30:01 -0700

    Refactor away page_title_from_query_object()
    The function's name was not accurate anymore, and it was only used from
    one place so it didn't need to be in the Utils module.
    
    [endlessm/eos-sdk#3559]

--- js/app/modules/searchBannerModule.js
+++ js/app/modules/searchBannerModule.js
@@ -10,7 +10,6 @@ const Actions = imports.app.actions;
 const Config = imports.app.config;
 const Dispatcher = imports.app.dispatcher;
 const Module = imports.app.interfaces.module;
-const Utils = imports.app.utils;
 
 String.prototype.format = Format.format;
 let _ = Gettext.dgettext.bind(null, Config.GETTEXT_PACKAGE);
@@ -33,23 +32,34 @@ const SearchBannerModule = new Lang.Class({
         Dispatcher.get_default().register((payload) => {
             switch(payload.action_type) {
                 case Actions.SEARCH_STARTED:
-                    /* TRANSLATORS: This message is displayed while the
-                    encyclopedia app is searching for results. The %s will be
-                    replaced with the term that the user searched for. Note, in
-                    English, it is surrounded by Unicode left and right double
-                    quotes (U+201C and U+201D). Make sure to include %s in your
-                    translation and use whatever quote marks are appropriate for
-                    your language. */
-                    this.label = _("Searching for “%s”").format('<span weight="normal" color="black">' +
-                        payload.query + '</span>');
+                    /* TRANSLATORS: This message is displayed while an app is
+                    searching for results. The %s will be replaced with the term
+                    that the user searched for. Note, in English, it is
+                    surrounded by Unicode left and right double quotes (U+201C
+                    and U+201D). Make sure to include %s in your translation and
+                    use whatever quote marks are appropriate for your language. */
+                    this.label = this._format_ui_string(_("Searching for “%s”"),
+                        payload.query);
                     break;
                 case Actions.SEARCH_READY:
-                    this.label = Utils.page_title_from_query_object(payload.query);
+                    /* TRANSLATORS: This message is displayed when an app is
+                    done searching for results. The %s will be replaced with the
+                    term that the user searched for. Note, in English, it is
+                    surrounded by Unicode left and right double quotes (U+201C
+                    and U+201D). Make sure to include %s in your translation and
+                    use whatever quote marks are appropriate for your language. */
+                    this.label = this._format_ui_string(_("Search results for “%s”"),
+                        payload.query);
                     break;
                 case Actions.SEARCH_FAILED:
                     this.label = _("OOPS!");
                     break;
             }
         });
+    },
+
+    _format_ui_string: function (ui_string, query) {
+        return ui_string.format('<span weight="normal" color="black">' + query +
+            '</span>');
     },
 });
