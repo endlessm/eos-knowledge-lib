8928f4a46184de709118e42aff8e760e859dcb31
Philip Chimento <philip@endlessm.com>
2015-10-14 15:40:09 -0700

    ScrollingTemplate for large views
    This adds a ScrollingTemplate which is basically a GtkScrolledWindow plus
    a GtkViewport. It's intendend for large views that don't scroll
    themselves, such as the highlights module and the thematic module in the
    travel app.
    
    [endlessm/eos-sdk#3699]

---
+++ js/app/modules/scrollingTemplate.js
@@ -0,0 +1,43 @@
+// Copyright 2015 Endless Mobile, Inc.
+
+/* exported ScrollingTemplate */
+
+const GObject = imports.gi.GObject;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+
+const Module = imports.app.interfaces.module;
+
+/**
+ * Class: ScrollingTemplate
+ * Layout template that allows its content to scroll vertically
+ *
+ * Slots:
+ *   content - where to put the content
+ */
+const ScrollingTemplate = new Lang.Class({
+    Name: 'ScrollingTemplate',
+    GTypeName: 'EknScrollingTemplate',
+    Extends: Gtk.ScrolledWindow,
+    Implements: [ Module.Module ],
+
+    Properties: {
+        'factory': GObject.ParamSpec.override('factory', Module.Module),
+        'factory-name': GObject.ParamSpec.override('factory-name', Module.Module),
+    },
+
+    Template: 'resource:///com/endlessm/knowledge/widgets/scrollingTemplate.ui',
+    InternalChildren: [ 'viewport' ],
+
+    _init: function (props={}) {
+        this.parent(props);
+
+        let content = this.create_submodule('content');
+        this._viewport.add(content);
+    },
+
+    // Module override
+    get_slot_names: function () {
+        return ['content'];
+    },
+});
