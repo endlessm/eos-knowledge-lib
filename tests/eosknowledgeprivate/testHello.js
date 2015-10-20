22c0de411550b34746d62370e2479adaa6df5066
Matt Watson <mattdangerw@gmail.com>
2015-10-19 18:46:07 -0700

    eosknowledgeprivate: remove example files
    We have real C files and real C tests, we don't need to keep the
    hello examples around
    [endlessm/eos-sdk#3747]

--- tests/eosknowledgeprivate/testHello.js
+++
@@ -1,7 +0,0 @@
-const EosKnowledgePrivate = imports.gi.EosKnowledgePrivate;
-
-describe('Sample C test', function () {
-    it('returns an appropriate greeting', function () {
-        expect(EosKnowledgePrivate.hello_c_provider_get_greeting()).toMatch('C');
-    });
-});
