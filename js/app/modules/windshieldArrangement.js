731b1db5938099deae6b074de52629b69c991dd1
Fernando Farfan <fernando@endlessm.com>
2015-10-23 10:24:39 -0600

    Added WindshieldArrangement
    Added the Windshield arrangement, an arrangement that displays a highlighted
    card prominently, along with three support cards at the bottom of the
    arrangement.
    
    [endlessm/eos-sdk#3695]

---
+++ js/app/modules/windshieldArrangement.js
@@ -0,0 +1,157 @@
+// Copyright 2015 Endless Mobile, Inc.
+
+/* exported WindshieldArrangement */
+
+const Cairo = imports.gi.cairo;
+const Endless = imports.gi.Endless;
+const GObject = imports.gi.GObject;
+const GLib = imports.gi.GLib;
+const Lang = imports.lang;
+
+const Arrangement = imports.app.interfaces.arrangement;
+const Card = imports.app.interfaces.card;
+const Module = imports.app.interfaces.module;
+
+const SECOND_ROW_CARD_COUNT = 3;
+const CARD_SIZE_SMALL = Card.MinSize.B;
+const CARD_SIZE_BIG = Card.MinSize.C;
+const CARD_SIZE_MAX = Card.MinSize.D;
+
+/**
+ * Class: WindshieldArrangement
+ * Arrangement with featured card on prominent spot and three supporting cards
+ *
+ * This arrangement shows a featured card in a very prominent spot, followed by
+ * three secondary cards.
+ */
+const WindshieldArrangement = new Lang.Class({
+    Name: 'WindshieldArrangement',
+    GTypeName: 'EknWindshieldArrangement',
+    Extends: Endless.CustomContainer,
+    Implements: [ Module.Module, Arrangement.Arrangement ],
+
+    Properties: {
+        'factory': GObject.ParamSpec.override('factory', Module.Module),
+        'factory-name': GObject.ParamSpec.override('factory-name', Module.Module),
+        /**
+         * Property: spacing
+         * The amount of space in pixels between children cards
+         *
+         * Default:
+         *   0
+         */
+        'spacing': GObject.ParamSpec.uint('spacing', 'Spacing',
+            'The amount of space in pixels between children cards',
+            GObject.ParamFlags.READWRITE,
+            0, GLib.MAXUINT16, 0),
+    },
+
+    _init: function (props={}) {
+        this._small_mode = false;
+        this._spacing = 0;
+
+        this.parent(props);
+    },
+
+    add_card: function (widget) {
+        this.add(widget);
+    },
+
+    get_cards: function () {
+        return this.get_children();
+    },
+
+    clear: function () {
+        this.get_children().forEach((child) => this.remove(child));
+    },
+
+    vfunc_get_preferred_width: function () {
+        return [this._get_size_with_spacing(CARD_SIZE_SMALL, SECOND_ROW_CARD_COUNT),
+            this._get_size_with_spacing(CARD_SIZE_MAX, SECOND_ROW_CARD_COUNT)];
+    },
+
+    vfunc_get_preferred_height: function () {
+        let card_size = CARD_SIZE_SMALL * (this._small_mode ? SECOND_ROW_CARD_COUNT - 1 : SECOND_ROW_CARD_COUNT);
+        let height = card_size + this._spacing;
+        return [height, height];
+    },
+
+    vfunc_size_allocate: function (alloc) {
+        this.parent(alloc);
+
+        // If arrangement has no child cards yet, simply exit
+        if (this.get_children().length === 0)
+            return;
+
+        this._small_mode = (alloc.width < this._get_size_with_spacing(CARD_SIZE_BIG, SECOND_ROW_CARD_COUNT));
+
+        let available_width = alloc.width - ((SECOND_ROW_CARD_COUNT - 1) * this._spacing);
+        let available_height = alloc.height - this._spacing;
+
+        let featured_height_factor = this._small_mode ? 0.5 : 2 / 3;
+        let featured_height = Math.floor(available_height * featured_height_factor);
+        let child_width = Math.floor(available_width / SECOND_ROW_CARD_COUNT);
+        let child_height = Math.floor(available_height * (1 - featured_height_factor));
+        let delta_x = child_width + this._spacing;
+
+        // Calculate spare pixels
+        // The floor operation we do above may lead us to have 1,2 spare pixels
+        let spare_pixels = alloc.width - (this._get_size_with_spacing(child_width, SECOND_ROW_CARD_COUNT));
+        let all_children = this.get_children();
+
+        // Featured card:
+        // Place the featured card at the at top of the arrangement
+        // FIXME: For now, we show the first-added card as the featured card.
+        // This should change to have a model with the featured flag added in
+        // this spot, but for that we'd need the arrangement interface to
+        // receive a model, instead of a widget in the "add" method.
+        let featured_card = all_children[0];
+        let featured_card_alloc = new Cairo.RectangleInt({
+            x: alloc.x,
+            y: alloc.y,
+            width: alloc.width,
+            height: featured_height,
+        });
+        featured_card.set_child_visible(true);
+        featured_card.size_allocate(featured_card_alloc);
+
+        let x = alloc.x;
+        let y = alloc.y + featured_height + this._spacing;
+
+        // Support cards:
+        // Place three support cards in a row below the featured cards
+        all_children.slice(1, SECOND_ROW_CARD_COUNT + 1).forEach((card, i) => {
+            card.set_child_visible(true);
+            let child_alloc = new Cairo.RectangleInt({
+                x: x,
+                y: y,
+                width: child_width,
+                height: child_height,
+            });
+            card.size_allocate(child_alloc);
+            x += delta_x + (i < spare_pixels ? 1 : 0);
+        });
+
+        // Additional cards:
+        // Should not be visible!
+        all_children.slice(SECOND_ROW_CARD_COUNT + 1, all_children.length).forEach((card) => {
+            card.set_child_visible(false);
+        });
+    },
+
+    get spacing() {
+        return this._spacing;
+    },
+
+    set spacing(value) {
+        if (this._spacing === value)
+            return;
+        this._spacing = value;
+        this.notify('spacing');
+        this.queue_resize();
+    },
+
+    _get_size_with_spacing: function (size, count) {
+        return size * count + this._spacing * (count - 1);
+    },
+});
---
+++ tests/js/app/modules/testWindshieldArrangement.js
@@ -0,0 +1,98 @@
+// Copyright 2015 Endless Mobile, Inc.
+
+const Gtk = imports.gi.Gtk;
+
+const Minimal = imports.tests.minimal;
+const MockWidgets = imports.tests.mockWidgets;
+const Utils = imports.tests.utils;
+const WidgetDescendantMatcher = imports.tests.WidgetDescendantMatcher;
+const WindshieldArrangement = imports.app.modules.windshieldArrangement;
+
+Gtk.init(null);
+
+describe('Windshield Arrangement', function () {
+    beforeEach(function () {
+        jasmine.addMatchers(WidgetDescendantMatcher.customMatchers);
+        this.arrangement = new WindshieldArrangement.WindshieldArrangement({
+            vexpand: false,
+        });
+    });
+
+    it('constructs', function () {
+        expect(this.arrangement).toBeDefined();
+    });
+
+    Minimal.test_arrangement_compliance();
+
+    describe('sizing allocation', function () {
+        // At 2000x2000, the featured card should be 2000x400, and the children
+        // cards should be 666x200.
+        testSizingArrangementForDimensions(2000, 600, 666);
+
+        // At 1200x1200, the featured card should be 1200x400, and the children
+        // cards should be 400x200.
+        testSizingArrangementForDimensions(1200, 1200, 400);
+
+        // At 1000x1000, the featured card should be 1000x400, and the children
+        // cards should be 333x200.
+        testSizingArrangementForDimensions(1000, 1000, 333);
+
+        // At 900x900, the featured card should be 900x400, and the children
+        // cards should be 300x200.
+        testSizingArrangementForDimensions(900, 900, 300);
+
+        // At 800x600, the featured card should be 800x200, and the children
+        // cards should be 266x200.
+        testSizingArrangementForDimensions(800, 600, 266);
+
+        // At 600x400, the featured card should be 600x200, and the children
+        // cards should be 200x200.
+        testSizingArrangementForDimensions(600, 400, 200);
+    });
+});
+
+function testSizingArrangementForDimensions(total_width, total_height, child_width) {
+    let win, add_card, cards;
+
+    beforeEach(function () {
+        add_card = (card) => {
+            card.show_all();
+            this.arrangement.add(card);
+            return card;
+        };
+        cards = [];
+
+        for (let i = 0; i < 5; i++) {
+            cards.push(add_card(new MockWidgets.TestBox(2000)));
+        }
+        win = new Gtk.OffscreenWindow();
+    });
+
+    afterEach(function () {
+        win.destroy();
+    });
+
+    it ('handles arrangement with dimensions ' + total_width + 'x' + total_height, function () {
+        win.add(this.arrangement);
+        win.set_size_request(total_width, total_height);
+        win.show_all();
+
+        win.queue_resize();
+        Utils.update_gui();
+
+        this.arrangement.get_cards().forEach((card, i) => {
+            if (i === 0) {
+                // FIXME: For now we're treating the first card as the featured card.
+                expect(card.get_child_visible()).toBe(true);
+                expect(card.get_allocation().width).toBe(total_width);
+            } else if (i < 4) {
+                // Three supporting child cards should be visible
+                expect(card.get_child_visible()).toBe(true);
+                expect(card.get_allocation().width).toBe(child_width);
+            } else {
+                // Additional cards should not be visible
+                expect(card.get_child_visible()).toBe(false);
+            }
+        });
+    });
+}
