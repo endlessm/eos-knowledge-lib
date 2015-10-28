50156ee36a05dfd501c85b063efd289db78b52d3
Fernando Farfan <fernando@endlessm.com>
2015-10-28 10:02:50 -0600

    Added ColoredBox
    Added ColoredBox, a widget that takes a random color and displays its dimensions
    in its label. Very useful for smoke tests!
    
    [endlessm/eos-sdk#3695]

--- tests/smoke-tests/squareGuysArrangementSmokeTest.js
+++ tests/smoke-tests/squareGuysArrangementSmokeTest.js
@@ -1,35 +1,9 @@
-const GLib = imports.gi.GLib;
 const Gtk = imports.gi.Gtk;
-const Lang = imports.lang;
 
+const ColoredBox = imports.tests.coloredBox;
 const SquareGuysArrangement = imports.app.modules.squareGuysArrangement;
 
 Gtk.init(null);
-
-const Box = new Lang.Class({
-    Name: 'Box',
-    Extends: Gtk.Frame,
-
-    _init: function (props={}) {
-        this.parent(props);
-
-        this._label = new Gtk.Label();
-        this.add(this._label);
-
-        let context = this.get_style_context();
-        let colors = ['fce94f', 'fcaf3e', 'e9b96e', '8ae234', '729fcf',
-            'ad7fa8', 'ef2929', '888a85'];
-        let color = colors[GLib.random_int_range(0, colors.length)];
-        let provider = new Gtk.CssProvider();
-        provider.load_from_data('*{background-color:#' + color + ';}');
-        context.add_provider(provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
-    },
-
-    vfunc_size_allocate: function (alloc) {
-        this.parent(alloc);
-        this._label.label = alloc.width + 'x' + alloc.height;
-    },
-});
 
 let win = new Gtk.Window();
 let arrangement = new SquareGuysArrangement.SquareGuysArrangement({
@@ -39,7 +13,7 @@ let arrangement = new SquareGuysArrangement.SquareGuysArrangement({
 });
 
 for (let i = 0; i < 10; i++) {
-    let card = new Box({
+    let card = new ColoredBox.ColoredBox({
         expand: true,
     });
     arrangement.add(card);
