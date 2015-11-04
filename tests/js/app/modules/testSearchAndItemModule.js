ead5177043e88843c995b879daa2bb56cd3615af
Matt Watson <mattdangerw@gmail.com>
2015-11-03 18:26:20 -0800

    searchAndItemModule: combined module
    A module that will flip between a SearchModule and ItemGroupModule.
    For use of the preset B article page.
    [endlessm/eos-sdk#3561]

---
+++ tests/js/app/modules/testSearchAndItemModule.js
@@ -0,0 +1,60 @@
+const Gtk = imports.gi.Gtk;
+
+Gtk.init(null);
+
+const Actions = imports.app.actions;
+const Minimal = imports.tests.minimal;
+const MockDispatcher = imports.tests.mockDispatcher;
+const MockFactory = imports.tests.mockFactory;
+const SearchAndItemModule = imports.app.modules.searchAndItemModule;
+const WidgetDescendantMatcher = imports.tests.WidgetDescendantMatcher;
+
+describe('Search and item module', function () {
+    let module, search, item, factory, dispatcher;
+
+    beforeEach(function () {
+        jasmine.addMatchers(WidgetDescendantMatcher.customMatchers);
+        dispatcher = MockDispatcher.mock_default();
+
+        factory = new MockFactory.MockFactory();
+        factory.add_named_mock('test-search', Minimal.MinimalArrangement);
+        factory.add_named_mock('test-item', Minimal.MinimalArrangement);
+        factory.add_named_mock('module', SearchAndItemModule.SearchAndItemModule, {
+            'item': 'test-item',
+            'search': 'test-search',
+        });
+        module = new SearchAndItemModule.SearchAndItemModule({
+            factory: factory,
+            factory_name: 'module',
+        });
+        module.show_all();
+        search = factory.get_created_named_mocks('test-search')[0];
+        item = factory.get_created_named_mocks('test-item')[0];
+    });
+
+    it('constructs', function () {
+        expect(module).toBeDefined();
+    });
+
+    it('creates and packs item module', function () {
+        expect(module).toHaveDescendant(item);
+    });
+
+    it('creates and packs search module', function () {
+        expect(module).toHaveDescendant(search);
+    });
+
+    it('shows the item module after set ready dispatched', function () {
+        dispatcher.dispatch({
+            action_type: Actions.SET_READY,
+        });
+        expect(module.visible_child).toBe(item);
+    });
+
+    it('shows the search module after search ready dispatched', function () {
+        dispatcher.dispatch({
+            action_type: Actions.SEARCH_READY,
+        });
+        expect(module.visible_child).toBe(search);
+    });
+});
