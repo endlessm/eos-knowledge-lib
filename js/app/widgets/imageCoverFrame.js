09b9cd5eb34056787c36f4b9d642c7c88c0347db
Matt Watson <mattdangerw@gmail.com>
2015-11-24 15:42:37 -0800

    imageCoverFrame: only load pixbuf when needed
    Instead of loading the pixbuf as soon as the content is set. Since
    we are creating way more cards then we actually display at the
    moment, this dramatically reduces memory usage.
    [endlessm/eos-sdk#3861]

--- js/app/widgets/imageCoverFrame.js
+++ js/app/widgets/imageCoverFrame.js
@@ -1,6 +1,5 @@
 const Gdk = imports.gi.Gdk;
 const GdkPixbuf = imports.gi.GdkPixbuf;
-const GObject = imports.gi.GObject;
 const Gtk = imports.gi.Gtk;
 const Lang = imports.lang;
 
@@ -23,32 +22,20 @@ const ImageCoverFrame = Lang.Class({
         this.set_has_window(false);
 
         this._aspect = 1.0;
-        this._natural_width = 0;
-        this._natural_height = 0;
+        this._stream = null;
         this._pixbuf = null;
     },
 
     set_content: function (stream) {
-        if (stream === null) {
-            this._pixbuf = null;
-            return;
-        }
-
-        this._pixbuf = GdkPixbuf.Pixbuf.new_from_stream(stream, null);
-
-        this._natural_width = this._pixbuf.get_width();
-        this._natural_height = this._pixbuf.get_height();
-        this._aspect = this._natural_width / this._natural_height;
-
+        this._stream = stream;
         this.queue_draw();
     },
 
-    vfunc_get_preferred_width: function () {
-        return [2, Math.max(this._natural_width, 2)];
-    },
-
-    vfunc_get_preferred_height: function () {
-        return [2, Math.max(this._natural_height, 2)];
+    _ensure_pixbuf: function () {
+        if ((!this._stream) || this._pixbuf)
+            return;
+        this._pixbuf = GdkPixbuf.Pixbuf.new_from_stream(this._stream, null);
+        this._aspect = this._pixbuf.get_width() / this._pixbuf.get_height();
     },
 
     // Scales the image to the right dimensions so that it covers the
@@ -61,8 +48,9 @@ const ImageCoverFrame = Lang.Class({
     },
 
     _draw_scaled_pixbuf: function (cr) {
-        if (this._pixbuf === null)
+        if (!this._stream)
             return;
+        this._ensure_pixbuf();
         let allocation = this.get_allocation();
         let [width, height] = this.get_scaled_dimensions(this._aspect, allocation.width, allocation.height);
         let scaled_pixbuf = this._pixbuf.scale_simple(width, height, GdkPixbuf.InterpType.BILINEAR);
