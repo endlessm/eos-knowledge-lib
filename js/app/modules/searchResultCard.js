0fc1f8b08316c4b26ec4c5f0621c66f96f1fb32a
Rory MacQueen <rorymacqueen@gmail.com>
2015-12-09 13:52:58 -0200

    Make sure synopsis label on search result card is always visible
    Some cards, most obviously those backed by set models,
    do not have synopsis. Normally in the card interface,
    when no synopsis is given, the synopsis label
    is hidden. In the case of search result cards
    however, we dont want this to happen since it will mess
    up the layout of widgets on the card. So lets force
    it to always be visible.
    
    [endlessm/eos-sdk#3863]

--- js/app/modules/searchResultCard.js
+++ js/app/modules/searchResultCard.js
@@ -41,6 +41,7 @@ const SearchResultCard = new Lang.Class({
         this.set_label_or_hide(this._synopsis_label, this.model.synopsis);
         this.set_label_with_highlight(this._synopsis_label, this.model.synopsis);
         this.set_size_request(Card.MinSize.E, Card.MinSize.A);
+        this._synopsis_label.visible = true;
 
         Utils.set_hand_cursor_on_widget(this);
     },
