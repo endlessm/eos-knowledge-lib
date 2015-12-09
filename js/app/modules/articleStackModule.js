60361675da7c1b9652a8439c9da963565132805b
Fernando Farfan <fernando@endlessm.com>
2015-12-09 17:12:52 -0500

    Homogeneize formatting of license tooltip
    When we introduced the new style of tooltips for all other apps, we used the
    default styling for the license tooltip. We now use the same style of license
    tooltip for all apps, and not only for reader apps.
    
    [endlessm/eos-sdk#3905]

--- js/app/modules/articleStackModule.js
+++ js/app/modules/articleStackModule.js
@@ -177,6 +177,13 @@ const ArticleStackModule = new Lang.Class({
             return Gdk.EVENT_STOP;
         }
 
+        if (GLib.uri_parse_scheme(uri) === 'file' && uri.indexOf('/licenses/') > -1) {
+            // If the uri has the "file://" scheme and it includes a segments for "licenses",
+            // it corresponds to a license file, and we should display it as an external link.
+            this._webview_tooltip_presenter.show_license_tooltip(tooltip);
+            return Gdk.EVENT_STOP;
+        }
+
         this._webview_tooltip_presenter.show_external_link_tooltip(tooltip, uri);
         return Gdk.EVENT_STOP;
      },
