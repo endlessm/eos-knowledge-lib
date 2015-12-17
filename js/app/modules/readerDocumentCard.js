30f78d766df8eddc8bb7de516620eac48f040328
Philip Chimento <philip@endlessm.com>
2015-12-17 14:07:48 -0800

    Ignore font overrides on composite
    Fancy fonts are often unreadable on composite TVs. When we are on one,
    we should ignore all the style knobs from overrides.css that have to do
    with fonts.
    
    [endlessm/eos-sdk#3933]

--- js/app/modules/readerDocumentCard.js
+++ js/app/modules/readerDocumentCard.js
@@ -192,9 +192,8 @@ function get_css_for_module (css_data, num) {
         str += Utils.object_to_css_string({'background-color': background_color}, '.document-card.variant' + num + ' .decorative-bar');
         delete css_data['title-background-color'];
     }
-    let title_data = Utils.get_css_for_submodule('title', css_data);
-    str += Utils.object_to_css_string(title_data, '.document-card.variant' + num + ' .title');
-    let module_data = Utils.get_css_for_submodule('module', css_data);
-    str += Utils.object_to_css_string(module_data, '.document-card.variant' + num + ' .attribution');
+    str += Utils.get_css_for_title_and_module(css_data,
+        '.document-card.variant' + num + ' .title',
+        '.document-card.variant' + num + ' .attribution');
     return str;
 }
