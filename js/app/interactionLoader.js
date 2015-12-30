51d9052b19a5f7550649919d91dcdd1d10304e52
Fernando Farfan <fernando@endlessm.com>
2015-12-30 12:25:26 -0500

    Clean up unused imports
    Clean up unused imports.
    
    [endlessm/eos-sdk#3679]

--- js/app/interactionLoader.js
+++ js/app/interactionLoader.js
@@ -1,7 +1,6 @@
 const EvinceDocument = imports.gi.EvinceDocument;
 const Gio = imports.gi.Gio;
 
-const Config = imports.app.config;
 const ModuleFactory = imports.app.moduleFactory;
 const Utils = imports.app.utils;
 
