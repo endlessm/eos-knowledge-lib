9a05a3e89bd354e179048e9d4e7f5a1c4220d2a3
Matt Watson <mattdangerw@gmail.com>
2016-01-13 15:45:28 -0800

    responsiveMarginsModule: refactor size_allocate
    This commit moves some hard coded constants into configurable
    properties, and more importantly does not cheat the gtk sizing model
    by setting margin on size allocate.
    [endlessm/eos-sdk#3976]

--- tests/js/app/modules/testResponsiveMarginsModule.js
+++ tests/js/app/modules/testResponsiveMarginsModule.js
@@ -20,15 +20,34 @@ describe('Responsive margins module', function () {
             factory: this.factory,
             factory_name: 'module',
         });
+
+        let provider = new Gtk.CssProvider();
+        provider.load_from_data('\
+        .tiny {\
+          margin: 0px;\
+        }\
+        .small {\
+          margin: 10px;\
+        }\
+        .medium {\
+          margin: 20px;\
+        }\
+        .large {\
+          margin: 30px;\
+        }\
+        .xlarge {\
+          margin: 40px;\
+        }');
+        this.responsive_margins.get_style_context().add_provider(provider, 800);
     });
 
     it('constructs', function () {});
 
     testMarginsForDimensions(720, 0);
-    testMarginsForDimensions(800, 40);
-    testMarginsForDimensions(1100, 62);
-    testMarginsForDimensions(1400, 83);
-    testMarginsForDimensions(1600, 120);
+    testMarginsForDimensions(800, 10);
+    testMarginsForDimensions(1100, 20);
+    testMarginsForDimensions(1400, 30);
+    testMarginsForDimensions(1600, 40);
 });
 
 function testMarginsForDimensions(total_width, margin) {
@@ -43,14 +62,15 @@ function testMarginsForDimensions(total_width, margin) {
     });
 
     it ('sets margins for internal child to ' + margin + 'px when width=' + total_width, function () {
+        let label = this.responsive_margins.get_children()[0];
+        label.expand = true;
         win.add(this.responsive_margins);
-        win.set_size_request(total_width, 100);
+        win.set_size_request(total_width, 600);
         win.show_all();
 
         win.queue_resize();
         Utils.update_gui();
 
-        expect(this.responsive_margins.get_children()[0].margin_start).toBe(margin);
-        expect(this.responsive_margins.get_children()[0].margin_end).toBe(margin);
+        expect(total_width).toBe(this.responsive_margins.get_allocated_width() + margin * 2);
     });
 }
