00729bbc8efb5d1ce47d9473b2323b7af413d15f
Fernando Farfan <fernando@endlessm.com>
2016-01-20 13:54:30 -0500

    Extract spare pixels computation into arrangement
    Extract the "get_spare_pixels_for_card_index" helper method into the Arrangement
    interface, so that it can be reused in other implementations.
    
    [endlessm/eos-sdk#3981]

--- js/app/interfaces/arrangement.js
+++ js/app/interfaces/arrangement.js
@@ -87,4 +87,30 @@ const Arrangement = new Lang.Interface({
             card.get_style_context().remove_class(StyleClasses.HIGHLIGHTED);
         }
     },
+
 });
+
+function get_spare_pixels_for_card_index (spare_pixels, cards_per_row, idx) {
+    if (spare_pixels === 0)
+        return 0;
+
+    // All gutters need an extra pixel
+    if (spare_pixels === cards_per_row - 1)
+        return 1;
+
+    let column = idx % cards_per_row;
+    let num_gutters = cards_per_row - 1;
+
+    // Assign a spare pixel to the center gutter if that helps keep things symmetric
+    if (num_gutters % 2 === 1 && spare_pixels % 2 === 1) {
+        if (column === (num_gutters - 1) / 2)
+            return 1;
+        spare_pixels--;
+    }
+    // Assign remaining spare pixels to alternating columns on either side
+    if (column < Math.ceil(spare_pixels / 2))
+        return 1;
+    if (column >= num_gutters - Math.floor(spare_pixels / 2))
+        return 1;
+    return 0;
+}
