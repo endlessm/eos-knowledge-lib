c125a3d74b9b97d42a918cef8a86d39c5addf08f
Rory MacQueen <rorymacqueen@gmail.com>
2016-02-05 16:56:13 -0200

    Rename TwoVertical to be TwoPiece
    The TwoVertical template is now the TwoPiece template.
    It can have both a vertical and a horizontal
    orientation.
    
    [endlessm/eos-sdk#3992]

---
+++ js/app/modules/twoPieceTemplate.js
@@ -0,0 +1,43 @@
+// Copyright 2016 Endless Mobile, Inc.
+
+const GObject = imports.gi.GObject;
+const Gtk = imports.gi.Gtk;
+const Lang = imports.lang;
+
+const Module = imports.app.interfaces.module;
+
+/**
+ * Class: TwoPieceTemplate
+ *
+ * Slots:
+ *   - first
+ *   - second
+ */
+const TwoPieceTemplate = new Lang.Class({
+    Name: 'TwoPieceTemplate',
+    GTypeName: 'EknTwoPieceTemplate',
+    Extends: Gtk.Grid,
+    Implements: [ Module.Module ],
+
+    Properties: {
+        'factory': GObject.ParamSpec.override('factory', Module.Module),
+        'factory-name': GObject.ParamSpec.override('factory-name', Module.Module),
+    },
+
+    _init: function (props={}) {
+        props.expand = true;
+        if (props.orientation === Gtk.Orientation.VERTICAL) {
+            props.halign = Gtk.Align.FILL;
+        } else if (props.orientation === Gtk.Orientation.HORIZONTAL) {
+            props.valign = Gtk.Align.FILL;
+        }
+        this.parent(props);
+
+        this.add(this.create_submodule('first'));
+        this.add(this.create_submodule('second'));
+    },
+
+    get_slot_names: function () {
+        return ['first', 'second'];
+    },
+});
