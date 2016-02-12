67c8035213380d0ffd2155ddb011a68de5d4c0ab
Philip Chimento <philip@endlessm.com>
2016-02-11 17:59:29 -0800

    Use scope instead of this in HalfArrangement test
    Using `this` for state was leftover from another way of doing this test.
    Removing it so that it's similar to our other tests is clearer.
    
    [endlessm/eos-sdk#3990]

--- tests/js/app/modules/testHalfArrangement.js
+++ tests/js/app/modules/testHalfArrangement.js
@@ -16,28 +16,76 @@ Compliance.test_arrangement_compliance(HalfArrangement.HalfArrangement);
 Compliance.test_arrangement_fade_in_compliance(HalfArrangement.HalfArrangement);
 
 describe('Half Arrangement', function () {
+    let factory, arrangement, win;
+
     beforeEach(function () {
-        let factory = new MockFactory.MockFactory();
+        factory = new MockFactory.MockFactory();
         factory.add_named_mock('card', Minimal.MinimalCard);
         factory.add_named_mock('arrangement', HalfArrangement.HalfArrangement, {
             'card-type': 'card',
         });
-        this.arrangement = factory.create_named_module('arrangement');
+        arrangement = factory.create_named_module('arrangement');
+
+        win = new Gtk.OffscreenWindow();
+        win.add(arrangement);
     });
 
-    describe('sizing allocation', function () {
-        beforeEach(function () {
+    afterEach(function () {
+        win.destroy();
+    });
+
+    function testSizingArrangementForDimensions(arrangement_size, card_width, card_height) {
+        it('handles arrangement with width=' + arrangement_size, function () {
             for (let i = 0; i < 5; i++)
-                this.arrangement.add_model(new ContentObjectModel.ContentObjectModel());
+                arrangement.add_model(new ContentObjectModel.ContentObjectModel());
 
-            this.win = new Gtk.OffscreenWindow();
-            this.win.add(this.arrangement);
+            win.set_size_request(arrangement_size, arrangement_size);
+            win.show_all();
+
+            win.queue_resize();
+            Utils.update_gui();
+
+            let all_cards = arrangement.get_children();
+            // Test featured cards
+            all_cards.slice(0, 2).forEach((card) => {
+                expect(card.get_child_visible()).toBe(true);
+                expect(card.get_allocation().width).toBe(arrangement_size / 2);
+                expect(card.get_allocation().height).toBe(Card.MinSize.C);
+            });
+
+            // Test support cards
+            all_cards.slice(2, all_cards.length).forEach((card) => {
+                expect(card.get_child_visible()).toBe(true);
+                expect(card.get_allocation().width).toBe(card_width);
+                expect(card.get_allocation().height).toBe(card_height);
+            });
         });
+    }
 
-        afterEach(function () {
-            this.win.destroy();
+    function testFeaturedCardsInArrangement(total_cards, featured_cards, featured_size, non_featured_size) {
+        it('treats ' + featured_cards + ' cards as featured when ' + total_cards + ' cards are added', function () {
+            for (let i = 0; i < total_cards; i++) {
+                arrangement.add_model(new ContentObjectModel.ContentObjectModel());
+            }
+
+            // In a 1000px wide arrangement, feature cards will be 500px wide
+            // and regular cards will be 333px wide.
+            win.set_size_request(1000, 500);
+            win.show_all();
+            win.queue_resize();
+            Utils.update_gui();
+
+            let cards = arrangement.get_children();
+            cards.slice(0, featured_cards).forEach((card) => {
+                expect(card.get_allocation().width).toBe(featured_size);
+            });
+            cards.slice(featured_cards).forEach((card) => {
+                expect(card.get_allocation().width).toBe(non_featured_size);
+            });
         });
+    }
 
+    describe('sizing allocation', function () {
         // At width=2000, featured cards should be 1000x300,
         // and the children cards should be 500x399.
         testSizingArrangementForDimensions(2000, 500, Card.MaxSize.C);
@@ -64,18 +112,6 @@ describe('Half Arrangement', function () {
     });
 
     describe('shows right number of featured cards', function () {
-        beforeEach(function () {
-            this.win = new Gtk.OffscreenWindow();
-            this.win.add(this.arrangement);
-            // In a 1000px wide arrangement, feature cards will be 500px wide
-            // and regular cards will be 333px wide.
-            this.win.set_size_request(1000, 500);
-        });
-
-        afterEach(function () {
-            this.win.destroy();
-        });
-
         // With 1 packed cards, 1 should be featured.
         testFeaturedCardsInArrangement(1, 1, 500, 0);
         // With 2 packed cards, 2 should be featured.
@@ -90,46 +126,3 @@ describe('Half Arrangement', function () {
         testFeaturedCardsInArrangement(10, 2, 500, 333);
     });
 });
-
-function testSizingArrangementForDimensions(arrangement_size, card_width, card_height) {
-    it('handles arrangement with width=' + arrangement_size, function () {
-        this.win.set_size_request(arrangement_size, arrangement_size);
-        this.win.show_all();
-
-        this.win.queue_resize();
-        Utils.update_gui();
-
-        let all_cards = this.arrangement.get_children();
-        // Test featured cards
-        all_cards.slice(0, 2).forEach((card) => {
-            expect(card.get_child_visible()).toBe(true);
-            expect(card.get_allocation().width).toBe(arrangement_size / 2);
-            expect(card.get_allocation().height).toBe(Card.MinSize.C);
-        });
-
-        // Test support cards
-        all_cards.slice(2, all_cards.length).forEach((card) => {
-            expect(card.get_child_visible()).toBe(true);
-            expect(card.get_allocation().width).toBe(card_width);
-            expect(card.get_allocation().height).toBe(card_height);
-        });
-    });
-}
-
-function testFeaturedCardsInArrangement(total_cards, featured_cards, featured_size, non_featured_size) {
-    it('treats ' + featured_cards + ' cards as featured when ' + total_cards + ' cards are added', function () {
-        for (let i = 0; i < total_cards; i++) {
-            this.arrangement.add_model(new ContentObjectModel.ContentObjectModel());
-        }
-        this.win.show_all();
-        this.win.queue_resize();
-        Utils.update_gui();
-
-        this.arrangement.get_children().slice(0, featured_cards).forEach((card) => {
-            expect(card.get_allocation().width).toBe(featured_size);
-        });
-        this.arrangement.get_children().slice(featured_cards).forEach((card) => {
-            expect(card.get_allocation().width).toBe(non_featured_size);
-        });
-    });
-}
