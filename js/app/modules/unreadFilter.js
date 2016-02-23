b5f81d9bc8fc17e2d29f69d68dc8ad7a28cae6b7
Rory MacQueen <rorymacqueen@gmail.com>
2016-02-23 15:12:10 -0300

    Add UnreadFilter
    Filters out read articles from an arrangement.
    
    [endlessm/eos-sdk#4001]

---
+++ js/app/modules/unreadFilter.js
@@ -0,0 +1,32 @@
+// Copyright 2016 Endless Mobile, Inc.
+
+/* exported UnreadFilter */
+
+const GObject = imports.gi.GObject;
+const Lang = imports.lang;
+
+const Filter = imports.app.interfaces.filter;
+const Module = imports.app.interfaces.module;
+const ReadingHistoryModel = imports.app.readingHistoryModel;
+
+const UnreadFilter = new Lang.Class({
+    Name: 'UnreadFilter',
+    GTypeName: 'EknUnreadFilter',
+    Extends: GObject.Object,
+    Implements: [ Module.Module, Filter.Filter ],
+
+    Properties: {
+        'factory': GObject.ParamSpec.override('factory', Module.Module),
+        'factory-name': GObject.ParamSpec.override('factory-name', Module.Module),
+        'invert': GObject.ParamSpec.override('invert', Filter.Filter),
+    },
+
+    _init: function (props={}) {
+        this.parent(props);
+    },
+
+    // Filter implementation
+    include_impl: function (model) {
+        return !ReadingHistoryModel.get_default().is_read_article(model.ekn_id);
+    },
+});
