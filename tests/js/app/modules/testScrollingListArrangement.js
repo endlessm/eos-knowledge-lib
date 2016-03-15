06f7969d80ad92faf375be092ceb441fb994b0f9
Matt Watson <mattdangerw@gmail.com>
2016-03-15 16:14:27 -0700

    listArrangement: split separate module with scroll
    We were using this in two contexts, one with scroll and one
    without. The scolled window that was "turned off" was causing
    problems and was kinda silly to begin with. Let's just split the
    modules.
    
    https://phabricator.endlessm.com/T10850

---
+++ tests/js/app/modules/testScrollingListArrangement.js
@@ -0,0 +1,12 @@
+const Gtk = imports.gi.Gtk;
+
+const Utils = imports.tests.utils;
+Utils.register_gresource();
+
+const Compliance = imports.tests.compliance;
+const ScrollingListArrangement = imports.app.modules.scrollingListArrangement;
+
+Gtk.init(null);
+
+Compliance.test_arrangement_compliance(ScrollingListArrangement.ScrollingListArrangement);
+Compliance.test_arrangement_fade_in_compliance(ScrollingListArrangement.ScrollingListArrangement);
