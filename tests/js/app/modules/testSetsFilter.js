79a45e09bc92ff7d244ec51450725624f8846cfc
Philip Chimento <philip@endlessm.com>
2016-04-13 17:53:05 -0700

    Add and use sets filter
    Instead of hardcoding SetPreviewCard and HighlightsModule to filter out
    sets, use a SetsFilter.
    
    https://phabricator.endlessm.com/T11172

---
+++ tests/js/app/modules/testSetsFilter.js
@@ -0,0 +1,45 @@
+// Copyright 2016 Endless Mobile, Inc.
+
+const ContentObjectModel = imports.search.contentObjectModel;
+const SetObjectModel = imports.search.setObjectModel;
+const SetsFilter = imports.app.modules.setsFilter;
+
+describe('Featured filter', function () {
+    const MODELS = [
+        new ContentObjectModel.ContentObjectModel({
+            tags: ['a', 'b', 'EknArticleObject'],
+        }),
+        new SetObjectModel.SetObjectModel({
+            tags: ['a', 'b', 'EknSetObject'],
+        }),
+    ];
+    let filter;
+
+    describe('normal mode', function () {
+        beforeEach(function () {
+            filter = new SetsFilter.SetsFilter();
+        });
+
+        it('is the default', function () {
+            expect(filter.invert).toBeFalsy();
+        });
+
+        it('filters out a regular model', function () {
+            expect(filter.include(MODELS[0])).toBeFalsy();
+            expect(filter.include(MODELS[1])).toBeTruthy();
+        });
+    });
+
+    describe('inverse mode', function () {
+        beforeEach(function () {
+            filter = new SetsFilter.SetsFilter({
+                invert: true,
+            });
+        });
+
+        it('filters out a set model', function () {
+            expect(filter.include(MODELS[0])).toBeTruthy();
+            expect(filter.include(MODELS[1])).toBeFalsy();
+        });
+    });
+});
