b01300367cd7c55ea9191d275e0b99f36e878acb
Rory MacQueen <rorymacqueen@gmail.com>
2016-05-06 19:03:28 -0300

    Add Moltres: app.json runner
    This script takes in a version 2 app.json, and
    runs it, providing fake data on the backend.
    
    It overwrites the normal Engine class with its
    own engine class which handles queries and
    direct object lookups.
    
    https://phabricator.endlessm.com/T11519

---
+++ js/app/moltresApplication.js
@@ -0,0 +1,55 @@
+imports.gi.versions.WebKit2 = '4.0';
+
+const Endless = imports.gi.Endless;
+const Gdk = imports.gi.Gdk;
+const Gio = imports.gi.Gio;
+const GObject = imports.gi.GObject;
+
+const ControllerLoader = imports.app.controllerLoader;
+const Knowledge = imports.app.knowledge;
+const MoltresEngine = imports.search.moltresEngine;
+
+/**
+ * Class: MoltresApplication
+ *
+ * A trimmed down application class for running test app.json files
+ */
+const MoltresApplication = new Knowledge.Class({
+    Name: 'MoltresApplication',
+    Extends: Endless.Application,
+
+    Properties: {
+        /**
+         * Property: app-json-path
+         * Path to applications app.json file.
+         */
+        'app-json-path': GObject.ParamSpec.string('app-json-path',
+            'App JSON Path', 'App JSON Path',
+            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY, ''),
+    },
+
+    _init: function (props={}) {
+        this.parent(props);
+        this._controller = null;
+    },
+
+    vfunc_activate: function () {
+        this.parent();
+        if (this._controller === null) {
+            let args = ['autobahn', this.app_json_path];
+            let subprocess = Gio.Subprocess.new(args, Gio.SubprocessFlags.STDOUT_PIPE);
+            let [victory, app_json] = subprocess.communicate_utf8(null, null);
+
+            MoltresEngine.override_engine();
+            // FIXME: The entire endless_knowledge theme is its 'custom css'.
+            // This will be fixed once we figure out theming for v2 apps.
+            let css_file = Gio.File.new_for_path('data/css/endless_knowledge.css');
+            let [success, data] = css_file.load_contents(null);
+            let css = data.toString();
+            this._controller = ControllerLoader.create_controller_with_app_json(this, JSON.parse(app_json), {
+                css: css,
+            });
+        }
+        this._controller.desktop_launch(Gdk.CURRENT_TIME);
+    },
+});
