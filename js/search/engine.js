b3b1abd8a115457ae21fc899c2661364e205c727
Matt Watson <mattdangerw@gmail.com>
2016-05-23 11:33:52 -0700

    compat: stop adding runtime sets https://phabricator.endlessm.com/T11174

--- js/search/engine.js
+++ js/search/engine.js
@@ -74,8 +74,6 @@ const Engine = Lang.Class({
         this._xapian_bridge = new XapianBridge.XapianBridge({ language: this.language });
 
         this._domain_cache = {};
-
-        this._runtime_objects = new Map();
     },
 
     /**
@@ -94,11 +92,6 @@ const Engine = Lang.Class({
     get_object_by_id: function (id, cancellable, callback) {
         let task = new AsyncTask.AsyncTask(this, cancellable, callback);
         task.catch_errors(() => {
-            if (this._runtime_objects.has(id)) {
-                task.return_value(this._runtime_objects.get(id));
-                return;
-            }
-
             let [domain, hash] = Utils.components_from_ekn_id(id);
             let domain_obj = this._get_domain(domain);
 
@@ -140,17 +133,6 @@ const Engine = Lang.Class({
     get_objects_by_query: function (query_obj, cancellable, callback) {
         let task = new AsyncTask.AsyncTask(this, cancellable, callback);
         task.catch_errors(() => {
-            // Currently, only objects with HOME_PAGE_TAG are overlaid at
-            // runtime. We cut a corner here and only check the runtime objects
-            // in that case, which saves us from having to check every single
-            // query against the set of overlaid runtime objects, which would be
-            // a speed-reducing backwards-compatibility nightmare.
-            if (this._runtime_objects.size > 0 && query_obj.tags.length === 1 &&
-                query_obj.tags[0] == HOME_PAGE_TAG) {
-                task.return_value([[...this._runtime_objects.values()], null]);
-                return;
-            }
-
             if (query_obj.domain === '')
                 query_obj = QueryObject.QueryObject.new_from_object(query_obj, { domain: this.default_domain });
 
@@ -204,26 +186,6 @@ const Engine = Lang.Class({
      */
     get_objects_by_query_finish: function (task) {
         return task.finish();
-    },
-
-    /**
-     * Method: add_runtime_object
-     * Add an object not stored in the database
-     *
-     * Creates an object with a particular ID, which is not stored in the
-     * database but behaves like it is.
-     * Effectively this allows modifying the database at runtime.
-     * This is necessary for compatibility reasons.
-     *
-     * Note that a runtime object with the same ID as an object stored in the
-     * database will override the stored object.
-     *
-     * Parameters:
-     *   id - EKN ID, of the form ekn://domain/sha
-     *   model - a <ContentObjectModel>
-     */
-    add_runtime_object: function (id, model) {
-        this._runtime_objects.set(id, model);
     },
 
     _get_domain: function (domain) {
