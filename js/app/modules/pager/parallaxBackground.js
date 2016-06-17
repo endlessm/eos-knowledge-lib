5ccc6affdbba6e32cea320da9aea9abeb8161824
Philip Chimento <philip@endlessm.com>
2016-06-17 17:01:32 -0400

    Remove extra provider in Pager.ParallaxBackground
    We still need to do disk I/O on the background image to find out its
    height/width ratio, unfortunately. But if we add CSS classes based on
    whether the pager's ratio is greater or less than the image's, then we
    can at least get rid of the extra CSS provider, which should improve
    performance a bit.
    
    https://phabricator.endlessm.com/T11992

--- js/app/modules/pager/parallaxBackground.js
+++ js/app/modules/pager/parallaxBackground.js
@@ -5,31 +5,27 @@
 const GdkPixbuf = imports.gi.GdkPixbuf;
 const Gio = imports.gi.Gio;
 const GObject = imports.gi.GObject;
-const Gtk = imports.gi.Gtk;
 
 const Module = imports.app.interfaces.module;
 const Simple = imports.app.modules.pager.simple;
 const Utils = imports.app.utils;
 
 /**
- * Defines how much the background slides when switching pages, and
- * therefore also how zoomed in the background image is from its
- * original size.
- */
-const PARALLAX_BACKGROUND_SCALE = 1.1;
-
-/**
  * Class: ParallaxBackground
  * Pager that provides a parallax effect on its page backgrounds
  *
  * FIXME: if GTK gains support for the 'vmax' CSS unit, then we can move this
- * effect to pure CSS and get rid of this module and its extra CSS provider.
+ * effect to pure CSS and get rid of this module and its extra disk I/O.
  * https://developer.mozilla.org/en-US/docs/Web/CSS/length
  *
  * CSS classes:
  *   PagerParallaxBackground--center - when on the appropriate page
+ *   PagerParallaxBackground--landscape - when the window is sized to a wider
+ *     ratio than the background image
  *   PagerParallaxBackground--left - when on the appropriate page
  *   PagerParallaxBackground--parallax - when parallax animations are enabled
+ *   PagerParallaxBackground--portrait - when the window is sized to a taller
+ *     ratio than the background image
  *   PagerParallaxBackground--right - when on the appropriate page
  */
 const ParallaxBackground = new Module.Class({
@@ -53,25 +49,24 @@ const ParallaxBackground = new Module.Class({
         this.parent(props);
 
         this._style_classes = {};
-        ['parallax', 'left', 'center', 'right'].forEach(modifier => {
+        ['parallax', 'left', 'center', 'right', 'portrait', 'landscape'].forEach(modifier => {
             this._style_classes[modifier] =
                 Utils.get_modifier_style_class(ParallaxBackground, modifier);
         });
 
+        this._landscape = true;
         let context = this.get_style_context();
-        this._bg_size_provider = new Gtk.CssProvider();
-        context.add_provider(this._bg_size_provider,
-            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION);
+        context.add_class(this._style_classes['landscape']);
 
         if (!Utils.low_performance_mode())
             context.add_class(this._style_classes['parallax']);
 
+        this._background_image_ratio = 1.0;
         if (this.background_image_uri) {
             try {
                 let stream = Gio.File.new_for_uri(this.background_image_uri).read(null);
                 let bg_pixbuf = GdkPixbuf.Pixbuf.new_from_stream(stream, null);
-                this._background_image_width = bg_pixbuf.width;
-                this._background_image_height = bg_pixbuf.height;
+                this._background_image_ratio = bg_pixbuf.width / bg_pixbuf.height;
             } catch (e) {
                 logError(e, 'Background image URI is not a valid format.');
             }
@@ -104,19 +99,19 @@ const ParallaxBackground = new Module.Class({
         if (Utils.low_performance_mode())
             return;
 
-        if (this.background_image_uri &&
-            (!this._last_allocation ||
-            this._last_allocation.width !== alloc.width ||
-            this._last_allocation.height !== alloc.height)) {
-            let bg_mult_ratio = Math.max(alloc.width / this._background_image_width,
-                alloc.height / this._background_image_height) *
-                PARALLAX_BACKGROUND_SCALE;
-            let bg_width = Math.ceil(this._background_image_width * bg_mult_ratio);
-            let bg_height = Math.ceil(this._background_image_height * bg_mult_ratio);
-            let frame_css = '* { background-size: ' + bg_width + 'px ' +
-                bg_height + 'px; }';
-            this._bg_size_provider.load_from_data(frame_css);
+        let ratio = alloc.width / alloc.height;
+        let landscape = ratio >= this._background_image_ratio;
+        if (landscape === this._landscape)
+            return;
+
+        let context = this.get_style_context();
+        if (landscape && !this._landscape) {
+            context.remove_class(this._style_classes['portrait']);
+            context.add_class(this._style_classes['landscape']);
+        } else {
+            context.remove_class(this._style_classes['landscape']);
+            context.add_class(this._style_classes['portrait']);
         }
-        this._last_allocation = alloc;
+        this._landscape = landscape;
     },
 });
