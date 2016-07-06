a105506c2e2b06818d0107159e1f2deae1da9b91
Philip Chimento <philip@endlessm.com>
2016-07-06 16:38:17 -0700

    Store hash state in Selection.SuggestedArticles
    This was previously referencing a this._query property that wasn't there.
    Fix this, and instead of storing the query anyway, store the hash
    computed from it.
    
    https://phabricator.endlessm.com/T11724

--- js/app/modules/selection/suggestedArticles.js
+++ js/app/modules/selection/suggestedArticles.js
@@ -16,6 +16,7 @@ const SuggestedArticles = new Module.Class({
 
     _init: function (props) {
         this.parent(props);
+        this._hash = 0;
         HistoryStore.get_default().connect('changed', this._on_history_changed.bind(this));
     },
 
@@ -26,17 +27,17 @@ const SuggestedArticles = new Module.Class({
     _TOTAL_ARTICLES: 50,
     construct_query_object: function (limit, query_index) {
         let item = HistoryStore.get_default().get_current_item();
-        if (!item)
+        if (!item || !item.query)
             throw new Error('This selection only works when there are search terms');
 
         if (query_index > 0)
             return null;
-        let hash = Utils.dumb_hash(item.query);
+        this._hash = Utils.dumb_hash(item.query);
         // FIXME: We still need a better way to issue a query for
         // 'random' articles. This just gets a random offset and then
         // requests articles (in order) starting from that point.
         let random_query = new QueryObject.QueryObject({
-            offset: hash % this._TOTAL_ARTICLES,
+            offset: this._hash % this._TOTAL_ARTICLES,
             limit: limit,
             tags_match_any: ['EknArticleObject'],
         });
@@ -50,10 +51,8 @@ const SuggestedArticles = new Module.Class({
         if (this._order) {
             models.sort(this._order.compare.bind(this._order));
         } else {
-            let hash = Utils.dumb_hash(this._query);
-
             // Reseed the pseudorandom function so that we get the same random sequence
-            GLib.random_set_seed(hash);
+            GLib.random_set_seed(this._hash);
 
             // Generate a pseudorandom sequence of numbers to use to shuffle the array
             let rand_sequence = Array.apply(null, {length: this._models_by_id.size}).map(GLib.random_double);
