41f521a7ca90cc02d85186a8301cef7792157325
Jasper St. Pierre <jstpierre@mecheye.net>
2016-07-20 18:14:55 -0700

    WIP - remove mw-editsection

--- data/templates/js/content-fixes.js
+++ data/templates/js/content-fixes.js
@@ -4,3 +4,5 @@ for (var i = 0; i < citationRequiredElements.length; i++)
 	if (citationRequiredElements[i].parentNode.parentNode.innerHTML[0] === "[")
 		citationRequiredElements[i].parentNode.parentNode.style.display = "none";
 }
+
+[].forEach.call(document.querySelectorAll('.mw-editsection'), function(elem) { elem.style.display = 'none'; });
