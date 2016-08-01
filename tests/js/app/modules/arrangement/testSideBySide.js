e8f50564586a2ad54c7db2589b271f279977e212
Rory MacQueen <rorymacqueen@gmail.com>
2016-08-01 20:33:37 +0000

    Add test coverage for all-visible property on arrangements
    Add unit tests to check for this property when applicable.
    For some arrangements, they have no limit on the number
    of cards that they show, in which case all-visible is
    always 'true', so we don't need tests for that.
    
    https://phabricator.endlessm.com/T11886

--- tests/js/app/modules/arrangement/testSideBySide.js
+++ tests/js/app/modules/arrangement/testSideBySide.js
@@ -45,13 +45,15 @@ describe('Arrangement.SideBySide', function () {
             win.destroy();
         });
 
-        function testSizingArrangementForDimensions(arr_width, visible_children, spacing) {
+        function testSizingArrangementForDimensions(arr_width, visible_children, spacing, all_visible) {
             let message = 'handles arrangement for width=' + arr_width;
             let x = 0;
 
             it (message, function () {
                 win.set_size_request(arr_width, 100);
                 Utils.update_gui();
+
+                expect(arrangement.all_visible).toBe(all_visible);
 
                 let cards = factory.get_created('card');
                 cards.forEach((card, i) => {
@@ -67,22 +69,22 @@ describe('Arrangement.SideBySide', function () {
         }
 
         // At width=2000px, all ten cards should be visible with 50px spacing
-        testSizingArrangementForDimensions(2000, 10, 50);
+        testSizingArrangementForDimensions(2000, 10, 50, true);
         // At width=1400px, all ten cards should be visible, with 40px spacing
-        testSizingArrangementForDimensions(1400, 10, 40);
+        testSizingArrangementForDimensions(1400, 10, 40, true);
         // At width=1200px, all ten cards should be visible, with 20px spacing
-        testSizingArrangementForDimensions(1200, 10, 20);
+        testSizingArrangementForDimensions(1200, 10, 20, true);
         // At width=1150px, all ten cards should be visible, with 15px spacing
-        testSizingArrangementForDimensions(1150, 10, 15);
+        testSizingArrangementForDimensions(1150, 10, 15, true);
         // At width=1000px, all ten cards should be visible, with 0px spacing
-        testSizingArrangementForDimensions(1000, 10, 0);
+        testSizingArrangementForDimensions(1000, 10, 0, true);
         // At width=900px, nine cards should be visible, with 0px spacing
-        testSizingArrangementForDimensions(900, 9, 0);
+        testSizingArrangementForDimensions(900, 9, 0, false);
         // At width=800px, eight cards should be visible, with 0px spacing
-        testSizingArrangementForDimensions(800, 8, 0);
+        testSizingArrangementForDimensions(800, 8, 0, false);
         // At width=720px, seven cards should be visible, with 0px spacing
-        testSizingArrangementForDimensions(720, 7, 0);
+        testSizingArrangementForDimensions(720, 7, 0, false);
         // At width=600px, six cards should be visible, with 0x spacing
-        testSizingArrangementForDimensions(600, 6, 0);
+        testSizingArrangementForDimensions(600, 6, 0, false);
     });
 });
