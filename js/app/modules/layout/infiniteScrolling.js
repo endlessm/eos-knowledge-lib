306701c176464f692a5d8bf2767cbaff1b520c58
Philip Chimento <philip@endlessm.com>
2016-08-02 18:31:51 -0700

    Remove scrolling capability from arrangements
    Previously, some arrangements had a built-in InfiniteScrolledWindow and
    others didn't. This makes things more consistent by removing scrolling
    capabilities from all arrangements.
    
    Instead, arrangements must be placed inside a Layout.InfiniteScrolling
    (or a Layout.Scrolling if you don't want infinite scrolling capability.)
    Layout.InfiniteScrolling now takes a 'lazy-load' module reference to a
    Selection, which it will request to load more models if the bottom buffer
    is reached.
    
    This removes Arrangement.ScrollingList since it would now otherwise be
    identical to Arrangement.List.
    
    https://phabricator.endlessm.com/T11724

--- js/app/modules/layout/infiniteScrolling.js
+++ js/app/modules/layout/infiniteScrolling.js
@@ -6,6 +6,8 @@ const InfiniteScrolledWindow = imports.app.widgets.infiniteScrolledWindow;
 const Module = imports.app.interfaces.module;
 const HistoryStore = imports.app.historyStore;
 
+const _BATCH_SIZE = 10;
+
 /**
  * Class: InfiniteScrolling
  * Layout template that allows its content to scroll vertically
@@ -20,6 +22,9 @@ const InfiniteScrolling = new Module.Class({
     Slots: {
         'content': {},
     },
+    References: {
+        'lazy-load': {},  // type: Selection
+    },
 
     Template: 'resource:///com/endlessm/knowledge/data/widgets/layout/infiniteScrolling.ui',
     InternalChildren: [ 'viewport' ],
@@ -31,6 +36,14 @@ const InfiniteScrolling = new Module.Class({
 
         this._viewport.add(content);
 
+        this.reference_module('lazy-load', selection => {
+            this._selection = selection;
+            this.connect('need-more-content',
+                this._on_need_more_content.bind(this));
+            this._selection.connect('models-changed',
+                this._on_models_changed.bind(this));
+        });
+
         HistoryStore.get_default().connect('changed', this._return_to_top.bind(this));
     },
 
@@ -40,5 +53,15 @@ const InfiniteScrolling = new Module.Class({
         if (this.vadjustment.get_value() !== lower) {
             this.vadjustment.set_value(lower);
         }
+    },
+
+    _on_need_more_content: function () {
+        if (this._selection.can_load_more)
+            this._selection.queue_load_more(_BATCH_SIZE);
+    },
+
+    _on_models_changed: function () {
+        if (this._selection.get_models().length > 0)
+            this.new_content_added();
     },
 });
