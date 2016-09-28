d446f85ffa935b726885e5ed1a0bebe177864846
Juan Pablo Ugarte <ugarte@endlessm.com>
2016-09-28 13:53:17 -0300

    EknMediaBin: added initial implementation of a media widget.
    EknMediaBin is a frontend widget for GStreamer playbin.
    
    Basic features implemented:
      o Audio/video playback support provided by GStreamer playbin plugin.
      o GTK video sink for output (will use gtkglsink if possible and fallback to gtksink)
      o UI controls implemented with regular Gtk+ widgets, themeable with CSS.
      o Play/Pause, Fullscreen/Unfullscreen and volume buttons.
      o Scale to control seeking and display playback position.
      o Various key bindings to control playback, seeking and entering/leaving fullscreen.
        (space, Right Left Up Down Page_Up Page_Down Home, f Escape)
    
    https://phabricator.endlessm.com/T13359

---
+++ examples/eos-player.c
@@ -0,0 +1,51 @@
+#include <gst/gst.h>
+#include <ekn-media-bin.h>
+
+static void
+on_title_notify (GObject *gobject, GParamSpec *pspec, GtkWidget *window)
+{
+  gtk_window_set_title (GTK_WINDOW (window),
+                        ekn_media_bin_get_title (EKN_MEDIA_BIN (gobject)));
+}
+
+int
+main (int argc, char **argv)
+{
+  GtkWidget *window, *bin;
+
+  gst_init (&argc, &argv);
+  gtk_init (&argc, &argv);
+
+  if (argc < 2)
+    {
+      g_print ("Usage: %s file:///full/path/to/media/file\n", argv[0]);
+      return 0;
+    }
+
+  /* Make sure animations are enabled, just in case we are running inside a VM */
+  g_object_set (gtk_settings_get_default (), "gtk-enable-animations", TRUE, NULL);
+
+  /* Create main window */
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+
+  /* Set some sane defaults */
+  gtk_window_set_default_size (GTK_WINDOW (window), 800, 450);
+  gtk_window_set_title (GTK_WINDOW (window), "Eos Player");
+  gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
+
+  /* Quit app on delete-event */
+  g_signal_connect (window, "delete-event", G_CALLBACK (gtk_main_quit), NULL);
+
+  bin = ekn_media_bin_new ();
+  ekn_media_bin_set_uri (EKN_MEDIA_BIN (bin), argv[1]);
+  gtk_container_add (GTK_CONTAINER (window), bin);
+
+  /* Update window title */
+  g_signal_connect (bin, "notify::title", G_CALLBACK (on_title_notify), window);
+
+  gtk_widget_show_all (window);
+
+  gtk_main ();
+
+  return 0;
+}
---
+++ lib/eosknowledgeprivate/ekn-media-bin.h
@@ -0,0 +1,73 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/*
+ * ekn-media-bin.h
+ *
+ * Copyright (C) 2016 Endless Mobile, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author: Juan Pablo Ugarte <ugarte@endlessm.com>
+ *
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define EKN_TYPE_MEDIA_BIN (ekn_media_bin_get_type ())
+G_DECLARE_FINAL_TYPE (EknMediaBin, ekn_media_bin, EKN, MEDIA_BIN, GtkBox)
+
+GtkWidget     *ekn_media_bin_new                  ();
+
+const gchar   *ekn_media_bin_get_uri              (EknMediaBin *self);
+void           ekn_media_bin_set_uri              (EknMediaBin *self,
+                                                   const gchar *uri);
+
+gdouble        ekn_media_bin_get_volume           (EknMediaBin *self);
+void           ekn_media_bin_set_volume           (EknMediaBin *self,
+                                                   gdouble      volume);
+
+gint           ekn_media_bin_get_autohide_timeout (EknMediaBin *self);
+void           ekn_media_bin_set_autohide_timeout (EknMediaBin *self,
+                                                   gint         autohide_timeout);
+
+gboolean       ekn_media_bin_get_fullscreen       (EknMediaBin *self);
+void           ekn_media_bin_set_fullscreen       (EknMediaBin *self,
+                                                   gboolean     fullscreen);
+
+gboolean       ekn_media_bin_get_show_stream_info (EknMediaBin *self);
+void           ekn_media_bin_set_show_stream_info (EknMediaBin *self,
+                                                   gboolean     show_stream_info);
+
+const gchar   *ekn_media_bin_get_title            (EknMediaBin *self);
+void           ekn_media_bin_set_title            (EknMediaBin *self,
+                                                   const gchar *title);
+
+const gchar   *ekn_media_bin_get_description      (EknMediaBin *self);
+void           ekn_media_bin_set_description      (EknMediaBin *self,
+                                                   const gchar *description);
+
+
+void           ekn_media_bin_play                 (EknMediaBin *self);
+void           ekn_media_bin_pause                (EknMediaBin *self);
+void           ekn_media_bin_stop                 (EknMediaBin *self);
+
+GdkPixbuf     *ekn_media_bin_screenshot           (EknMediaBin *self,
+                                                   gint         width,
+                                                   gint         height);
+
+G_END_DECLS
