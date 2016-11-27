5a342f52b6185a4dfdfb1ad767f6f6f97d8dec17
Matt Watson <mattdangerw@gmail.com>
2016-11-26 17:08:29 -0800

    ekncontent: make library public and parallel installable https://phabricator.endlessm.com/T13418

---
+++ ekncontent/ekncontent/eos-knowledge-content.h
@@ -0,0 +1,17 @@
+/* Copyright 2016 Endless Mobile, Inc. */
+
+#pragma once
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+#define _EKN_CONTENT_INSIDE_EOSKNOWLEDGECONTENT_H
+
+/* Pull in other header files */
+#include "eknc-utils.h"
+#include "eknc-subtree-dispatcher.h"
+
+#undef _EKN_CONTENT_INSIDE_EOSKNOWLEDGECONTENT_H
+
+G_END_DECLS
