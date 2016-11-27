9098e827d1a5edff0a6809966f0af372708fd52d
Matt Watson <mattdangerw@gmail.com>
2016-11-26 20:25:30 -0800

    ekncontent: add version macros https://phabricator.endlessm.com/T13418

--- ekncontent/ekncontent/eos-knowledge-content.h
+++ ekncontent/ekncontent/eos-knowledge-content.h
@@ -11,6 +11,7 @@ G_BEGIN_DECLS
 /* Pull in other header files */
 #include "eknc-utils.h"
 #include "eknc-subtree-dispatcher.h"
+#include "eknc-version.h"
 
 #undef _EKN_CONTENT_INSIDE_EOSKNOWLEDGECONTENT_H
 
