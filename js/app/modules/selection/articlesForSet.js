411a90a6e047a970e99638b79027a5f747eaa32b
Rory MacQueen <rorymacqueen@gmail.com>
2017-01-17 15:24:32 -0200

    Have ArticlesForSet subclass ContentForSet
    Since they basically do the same thing, except for the
    constructing the query itself.
    
    https://phabricator.endlessm.com/T14516

--- js/app/modules/selection/articlesForSet.js
+++ js/app/modules/selection/articlesForSet.js
@@ -4,27 +4,12 @@
 
 const Eknc = imports.gi.EosKnowledgeContent;
 
-const Actions = imports.app.actions;
-const Dispatcher = imports.app.dispatcher;
-const HistoryStore = imports.app.historyStore;
+const ContentForSet = imports.app.modules.selection.contentForSet;
 const Module = imports.app.interfaces.module;
-const Xapian = imports.app.modules.selection.xapian;
 
 const ArticlesForSet = new Module.Class({
     Name: 'Selection.ArticlesForSet',
-    Extends: Xapian.Xapian,
-
-    _init: function (props={}) {
-        this.parent(props);
-        if (this.global) {
-            this.model = HistoryStore.get_default().current_set;
-            HistoryStore.get_default().connect('notify::current-set', () => {
-                this.model = HistoryStore.get_default().current_set;
-                this._set_needs_refresh(true);
-            });
-        }
-        this._set_needs_refresh(true);
-    },
+    Extends: ContentForSet.ContentForSet,
 
     construct_query_object: function (limit, query_index) {
         if (query_index > 0)
@@ -38,14 +23,6 @@ const ArticlesForSet = new Module.Class({
             tags_match_any: this.model.child_tags,
             tags_match_all: ['EknArticleObject'],
             sort: Eknc.QueryObjectSort.SEQUENCE_NUMBER,
-        });
-    },
-
-    show_more: function () {
-        Dispatcher.get_default().dispatch({
-            action_type: Actions.ITEM_CLICKED,
-            model: this.model,
-            context_label: this.model.title,
         });
     },
 });
