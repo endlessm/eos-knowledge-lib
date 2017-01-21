5dec5c1fcecb2e1abeb8075ed124dee6af2b64f3
Matt Watson <mattdangerw@gmail.com>
2017-01-20 16:31:09 -0800

    testEngine: revive engine tests
    We can use our new mock content directory to actually enable some
    tests here.
    https://phabricator.endlessm.com/T14909

--- ekncontent/tests/ekncontent/testEngine.js
+++ ekncontent/tests/ekncontent/testEngine.js
@@ -1,37 +1,125 @@
 const Eknc = imports.gi.EosKnowledgeContent;
 const InstanceOfMatcher = imports.tests.InstanceOfMatcher;
 
-// Need to rework engine tests for the C version
-xdescribe('Engine', function () {
+describe('Engine', function () {
     let engine;
-    let mock_domain;
 
     beforeEach(function () {
         jasmine.addMatchers(InstanceOfMatcher.customMatchers);
-        engine = new Eknc.Engine();
-        engine.default_app_id = 'foo';
-
-        mock_domain = {
-            get_object: jasmine.createSpy(),
-            get_object_finish: jasmine.createSpy(),
-        };
-        spyOn(engine, '_get_domain').and.returnValue(mock_domain);
+        engine = Eknc.Engine.get_default();
     });
 
-    describe('domain wrap behavior', function () {
-        it('calls get_object_by_id correctly', function (done) {
-            mock_domain.get_object.and.callFake(function (id, cancellable, callback) {
-                callback(mock_domain, 'testing whether this was called');
+    describe('get_domain_for_app', function () {
+        it('returns a domain for an app id that exists', function () {
+            let domain = engine.get_domain_for_app('com.endlessm.fake_test_app.en');
+            expect(domain).not.toBe(null);
+            expect(domain).toBeA(Eknc.Domain);
+        });
+
+        it('throws for an app id that does not exist', function () {
+            expect(() => engine.get_domain_for_app('com.endlessm.invalid_app.en')).toThrow();
+        });
+    });
+
+    describe('get_object_for_app', function () {
+        it('returns a model for valid app id, ekn id pair', function (done) {
+            engine.get_object_for_app('ekn:///02463d24cb5690af2c8e898736ea8c80e0e77077',
+                                      'com.endlessm.fake_test_app.en',
+                                      null,
+                                      function (engine, result) {
+                let model = engine.get_object_finish(result);
+                expect(model).not.toBe(null);
+                expect(model).toBeA(Eknc.ArticleObjectModel);
+                done();
             });
-            mock_domain.get_object_finish.and.callFake(function (task) {
-                return task;
+        });
+
+        it('throws for an app id that does not exist', function (done) {
+            engine.get_object_for_app('ekn:///02463d24cb5690af2c8e898736ea8c80e0e77077',
+                                      'com.endlessm.invalid_app.en',
+                                      null,
+                                      function (engine, result) {
+                expect(() => engine.get_object_finish(result)).toThrow();
+                done();
+            });
+        });
+
+        it('throws for an app id that does not exist', function (done) {
+            engine.get_object_for_app('ekn:///0000000000000000000000000000000000000000',
+                                      'com.endlessm.fake_test_app.en',
+                                      null,
+                                      function (engine, result) {
+                expect(() => engine.get_object_finish(result)).toThrow();
+                done();
+            });
+        });
+    });
+
+    describe('test_link_for_app', function () {
+        it('returns an id for valid app id, link pair', function () {
+            let id = engine.test_link_for_app('https://en.wikipedia.org/wiki/America',
+                                              'com.endlessm.fake_test_app.en');
+            expect(id).not.toBe(null);
+        });
+
+        it('throws for an app id that does not exist', function () {
+            expect(() => {
+                engine.test_link_for_app('https://en.wikipedia.org/wiki/America',
+                                         'com.endlessm.invalid_app.en');
+            }).toThrow();
+        });
+
+        it('return null for an ekn id that does not exist', function () {
+            let id = engine.test_link_for_app('http://www.bbc.com/news/',
+                                              'com.endlessm.fake_test_app.en');
+            expect(id).toBe(null);
+        });
+    });
+
+    describe('with a default app id', function () {
+        beforeEach(function () {
+            engine.default_app_id = 'com.endlessm.fake_test_app.en';
+        });
+
+        describe('get_domain', function () {
+            it('returns the domain for the default id', function () {
+                let domain = engine.get_domain();
+                expect(domain).toBeA(Eknc.Domain);
+                expect(domain.app_id).toBe('com.endlessm.fake_test_app.en');
+            });
+        });
+
+        describe('get_object', function () {
+            it('returns a model for valid ekn id', function () {
+                engine.get_object('ekn:///02463d24cb5690af2c8e898736ea8c80e0e77077',
+                                  null,
+                                  function (engine, result) {
+                    let model = engine.get_object_finish(result);
+                    expect(model).not.toBe(null);
+                    expect(model).toBeA(Eknc.ArticleObjectModel);
+                });
             });
 
-            engine.get_object('ekn:///1234567890abcdef', null, function (engine, task) {
-                let res = engine.get_object_finish(task);
-                expect(res).toEqual('testing whether this was called');
-                expect(mock_domain.get_object).toHaveBeenCalled();
-                done();
+            it('throws for an app id that does not exist', function () {
+                engine.get_object('ekn:///0000000000000000000000000000000000000000',
+                                  null,
+                                  function (engine, result) {
+                    expect(() => engine.get_object_finish(result)).toThrow();
+                });
+            });
+        });
+
+        describe('test_link_for_app', function () {
+            it('returns an id a valid link', function () {
+                let id = engine.test_link_for_app('https://en.wikipedia.org/wiki/America',
+                                                  'com.endlessm.fake_test_app.en');
+                expect(id).not.toBe(null);
+            });
+
+            it('returns null for an invalid link', function () {
+                let id = engine.test_link_for_app('http://www.bbc.com/news/',
+                                                  'com.endlessm.fake_test_app.en');
+                expect(id).toBe(null);
             });
         });
     });
