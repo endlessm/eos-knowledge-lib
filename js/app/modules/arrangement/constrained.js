92669311eceb8f8e1f341977b5b6a4f9f1b98c40
katelk28 <katelk28@gmail.com>
2017-04-03 17:16:04 -0700

    WIP -- Figuring out infinite scroll

--- js/app/modules/arrangement/constrained.js
+++ js/app/modules/arrangement/constrained.js
@@ -11,10 +11,10 @@ const Card = imports.app.interfaces.card;
 const Module = imports.app.interfaces.module;
 
 const _DEFAULT_CARD_COUNT = 10;
-const _DEFAULT_COLUMNS = 5;
-const _DEFAULT_ROWS = 6;
+const _DEFAULT_COLUMNS = 1;
+const _DEFAULT_ROWS = 10;
 const _CARD_MIN_WIDTH = Card.MinSize.B;
-const _CARD_MIN_HEIGHT = Card.MinSize.B;
+const _CARD_MIN_HEIGHT = Card.MinSize.C;
 
 const _Placeholder = new Lang.Class({
     Name: 'Placeholder',
@@ -45,9 +45,9 @@ const Constrained = new Module.Class({
     Implements: [Arrangement.Arrangement],
 
     // Arrangement override
-    get_max_cards: function () {
-        return _DEFAULT_CARD_COUNT;
-    },
+    // get_max_cards: function () {
+    //     return _DEFAULT_CARD_COUNT;
+    // },
 
     // Arrangement override
     set_models: function (models) {
@@ -62,24 +62,6 @@ const Constrained = new Module.Class({
         this.remove(child);
     },
 
-    /**
-     * Method: get_description
-     * Get Emeus VFL description for this arrangement.
-     */
-    get_description: function () {
-        return [
-            'H:|[view0(view4 * 3)][view1(view4 * 2)]|',
-            'H:|[view0][view2(view4 * 2)]|',
-            'H:|[view3(view4 * 2)][view4][view2]|',
-            'H:|[view3][view5(view4 * 2)][view6(view4)]|',
-            'H:|[view8(view4)][view9(view4)][view5][view7(view4)]|',
-            'V:|[view0(view4 * 3)][view3(view4 * 2)][view8(view4)]|',
-            'V:[view3][view9(view4)]|',
-            'V:|[view0][view4][view5(view4 * 2)]|',
-            'V:|[view1(view4 * 2)][view2(view4 * 2)][view6(view4)][view7(view4)]|',
-        ];
-    },
-
     _setup_constraints: function () {
         // remove existing placeholders, if any
         let holders = this.get_children().filter(child => child.get_child() instanceof _Placeholder);
@@ -90,13 +72,26 @@ const Constrained = new Module.Class({
 
         // create widgets map with existing cards
         let widgets = {};
+        let descriptions = [];
+        let h_description = '';
+        let v_description = 'V:|';
         this.get_children().forEach((child, index) => {
-            widgets['view' + index] = child.get_child();
-        });
+            let name_current = 'view' + index;
+            widgets[name_current] = child.get_child();
 
-        // fill with placeholders in empty slots, if any
+            // add H description
+            h_description = 'H:|[' + name_current + '(view0)]|';
+            descriptions.push(h_description);
+
+            // add V description
+            v_description += '[' + name_current + '(view0)]';
+        });
+        v_description += '|';
+        descriptions.push(v_description);
+
+        // fill arrangement with placeholders in empty slots, if any
         let n_children = this.get_children().length;
-        for (let index = n_children; index < this.get_max_cards(); index++) {
+        for (let index = n_children; index < _DEFAULT_CARD_COUNT; index++) {
                 let holder = new _Placeholder();
                 widgets['view' + index] = holder;
                 this.add(holder);
@@ -105,12 +100,13 @@ const Constrained = new Module.Class({
         // clear existing constraints and re-create new ones
         this.clear_constraints();
         let constraints = Emeus.create_constraints_from_description(
-            this.get_description(),
+            descriptions,
             0, 0,
             widgets,
             {});
         constraints.forEach(this.add_constraint, this);
     },
+
 
     // XXX workaround for minimum size issues in Emeus
     vfunc_get_request_mode: function () {
@@ -122,6 +118,6 @@ const Constrained = new Module.Class({
     },
 
     vfunc_get_preferred_height: function () {
-        return [_CARD_MIN_HEIGHT * _DEFAULT_ROWS, _CARD_MIN_HEIGHT * _DEFAULT_ROWS];
+        return [_CARD_MIN_HEIGHT * this.get_children().length, _CARD_MIN_HEIGHT * this.get_children().length];
     },
 });
