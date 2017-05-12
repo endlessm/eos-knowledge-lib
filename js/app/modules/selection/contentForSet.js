7ce9f7b4c71ce441cc710bd13d3d7e6cd446ddad
Philip Chimento <philip@endlessm.com>
2017-05-11 19:17:21 -0700

    Selection.SubsetsForSet: Merge into Selection.ContentForSet
    If we move the track-subset and track-parent properties into
    Selection.ContentForSet, then we can replace Selection.SubsetsForSet with
    Selection.ContentForSet plus Filter.Sets.
    
    https://phabricator.endlessm.com/T13175

--- js/app/modules/selection/contentForSet.js
+++ js/app/modules/selection/contentForSet.js
@@ -3,27 +3,71 @@
 // Copyright 2016 Endless Mobile, Inc.
 
 const Eknc = imports.gi.EosKnowledgeContent;
+const GObject = imports.gi.GObject;
 
 const Actions = imports.app.actions;
 const Dispatcher = imports.app.dispatcher;
 const HistoryStore = imports.app.historyStore;
 const Module = imports.app.interfaces.module;
+const SetMap = imports.app.setMap;
 const Xapian = imports.app.modules.selection.xapian;
 
+/**
+ * Class: ContentForSet
+ * Selection that retrieves content related to the current set
+ */
 const ContentForSet = new Module.Class({
     Name: 'Selection.ContentForSet',
     Extends: Xapian.Xapian,
 
+    Properties: {
+        /**
+         * Property: track-subset
+         * Whether this content group should track changes in subset as
+         * opposed to top level set.
+         */
+        'track-subset': GObject.ParamSpec.boolean('track-subset',
+            'Track subset', 'Only refresh when subset changes',
+            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY,
+            false),
+        /**
+         * Property: track-parent
+         * Whether this selection should track changes in parent of the current
+         * set. This is useful when we have a page showing both content from
+         * the current set and also a navigation panel showing the siblings
+         * of the current set.
+         */
+        'track-parent': GObject.ParamSpec.boolean('track-parent',
+            'Track parent', 'Only refresh when parent changes',
+            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY,
+            false),
+    },
+
     _init: function (props={}) {
         this.parent(props);
         if (this.global) {
-            this.model = HistoryStore.get_default().current_set;
-            HistoryStore.get_default().connect('notify::current-set', () => {
-                this.model = HistoryStore.get_default().current_set;
+            let property_name = 'current-set';
+            if (this.track_subset) {
+                property_name = 'current-subset';
+            }
+
+            this.model = this._get_model(property_name);
+            HistoryStore.get_default().connect('notify::' + property_name, () => {
+                let model = this._get_model(property_name);
+                if (!model || (this.model && model.ekn_id === this.model.ekn_id))
+                    return;
+                this.model = model;
                 this._set_needs_refresh(true);
             });
         }
         this._set_needs_refresh(true);
+    },
+
+    _get_model: function (property_name) {
+        let model = HistoryStore.get_default()[property_name.replace('-', '_', 'g')];
+        if (model && this.track_parent)
+            model = SetMap.get_parent_set(model);
+        return model;
     },
 
     construct_query_object: function (limit, query_index) {
