76a8806c0e4a916a4803eb880526d286c3c98ea1
Juan Pablo Ugarte <ugarte@endlessm.com>
2017-05-22 19:22:20 +0000

    Card.Title: removed decorate-on-highlight property
    https://phabricator.endlessm.com/T16646

--- js/app/modules/card/title.js
+++ js/app/modules/card/title.js
@@ -12,9 +12,6 @@ const NavigationCard = imports.app.interfaces.navigationCard;
 const ThemeableImage = imports.app.widgets.themeableImage;
 const Utils = imports.app.utils;
 
-// The highlight decoration element is 5x5
-const HIGHLIGHT_DECORATION_DIMENSION = 5;
-
 /**
  * Class: Title
  *
@@ -34,15 +31,6 @@ const Title = new Module.Class({
     Implements: [Card.Card, NavigationCard.NavigationCard],
 
     Properties: {
-        /**
-         * Property: decorate-on-highlight
-         * Whether to draw a custom decoration when the card is highlighted
-         */
-        'decorate-on-highlight': GObject.ParamSpec.boolean('decorate-on-highlight',
-            'Decorate on highlight', 'Whether to draw a custom decoration when the card is highlighted',
-            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY,
-            false),
-
         /**
          * Property: max-title-lines
          *
@@ -68,32 +56,5 @@ const Title = new Module.Class({
         this._title_label_text = this._title_label.label;
 
         this._title_label.lines = this.max_title_lines;
-    },
-
-    vfunc_draw: function (cr) {
-        if (this.decorate_on_highlight && this.get_style_context().has_class('highlighted')) {
-            let x = this.get_allocation().width;
-            let y = this._title_label.get_allocation().height;
-
-            cr.save();
-            Gdk.cairo_set_source_rgba(cr, new Gdk.RGBA({
-                red: 1.0,
-                green: 1.0,
-                blue: 1.0,
-                alpha: 1.0,
-            }));
-            cr.moveTo(0, y);
-            cr.lineTo((x - HIGHLIGHT_DECORATION_DIMENSION) / 2, y);
-            cr.lineTo(x / 2, y + (HIGHLIGHT_DECORATION_DIMENSION / 2));
-            cr.lineTo((x + HIGHLIGHT_DECORATION_DIMENSION) / 2, y);
-            cr.lineTo(x, y);
-            cr.stroke();
-            cr.restore();
-        }
-
-        this.parent(cr);
-
-        cr.$dispose();
-        return Gdk.EVENT_PROPAGATE;
     },
 });
