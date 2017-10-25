5220834142ada83d191f24f9d6474f95f984f14c
Emmanuele Bassi <ebassi@gnome.org>
2017-10-25 16:40:14 +0100

    Ignore deprecation warnings for g_object_newv()
    Until we port to g_object_new_with_properties(), or we change the way we
    copy or create objects from JSON fragments.
    
    (cherry picked from commit 2b4c226d5f43ccdf7b0c3741cdcb2436c7d2f2d4)
    Signed-off-by: Emmanuele Bassi <ebassi@gnome.org>

--- ekncontent/ekncontent/eknc-image-object-model.c
+++ ekncontent/ekncontent/eknc-image-object-model.c
@@ -38,11 +38,13 @@ eknc_image_object_model_init (EkncImageObjectModel *self)
 EkncContentObjectModel *
 eknc_image_object_model_new_from_json_node (JsonNode *node)
 {
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
   GArray *params = g_array_new (FALSE, TRUE, sizeof (GParameter));
   eknc_media_object_model_add_json_to_params (node, params);
   EkncImageObjectModel *model = g_object_newv (EKNC_TYPE_IMAGE_OBJECT_MODEL,
                                                params->len,
                                                (GParameter *)params->data);
   eknc_utils_free_gparam_array (params);
+G_GNUC_END_IGNORE_DEPRECATIONS
   return EKNC_CONTENT_OBJECT_MODEL (model);
 }
--- ekncontent/ekncontent/eknc-media-object-model.c
+++ ekncontent/ekncontent/eknc-media-object-model.c
@@ -198,11 +198,13 @@ eknc_media_object_model_add_json_to_params (JsonNode *node,
 EkncContentObjectModel *
 eknc_media_object_model_new_from_json_node (JsonNode *node)
 {
+G_GNUC_BEGIN_IGNORE_DEPRECATIONS
   GArray *params = g_array_new (FALSE, TRUE, sizeof (GParameter));
   eknc_media_object_model_add_json_to_params (node, params);
   EkncMediaObjectModel *model = g_object_newv (EKNC_TYPE_MEDIA_OBJECT_MODEL,
                                                params->len,
                                                (GParameter *)params->data);
   eknc_utils_free_gparam_array (params);
+G_GNUC_END_IGNORE_DEPRECATIONS
   return EKNC_CONTENT_OBJECT_MODEL (model);
 }
