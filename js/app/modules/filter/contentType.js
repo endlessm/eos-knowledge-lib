5cd3557c68d92c926e732a8ab90be853c8271cb1
Martin Abente Lahaye <martin@endlessm.com>
2018-02-13 14:18:02 -0300

    modules: Add Filter.ContentType
    This filter includes only object models of the content type specified
    in its `content-type` string property.
    
    https://phabricator.endlessm.com/T21128

---
+++ js/app/modules/filter/contentType.js
@@ -0,0 +1,49 @@
+// Copyright 2018 Endless Mobile, Inc.
+
+/* exported ContentType */
+
+const Eknc = imports.gi.EosKnowledgeContent;
+const GObject = imports.gi.GObject;
+
+const Filter = imports.app.interfaces.filter;
+const Module = imports.app.interfaces.module;
+
+/**
+ * Class: ContentType
+ * Filter that includes only a content type
+ *
+ * This filter includes only object models of the content type
+ * specified in its `content-type` string property.
+ */
+var ContentType = new Module.Class({
+    Name: 'Filter.ContentType',
+    Extends: GObject.Object,
+    Implements: [Filter.Filter],
+
+    Properties: {
+        'content-type': GObject.ParamSpec.string('content-type', 'Content Type', 'Content Type to include',
+            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY, ''),
+    },
+
+    // Filter implementation
+    include_impl(model) {
+        return model.content_type.startsWith(this.content_type);
+    },
+
+    // Filter implementation
+    modify_xapian_query_impl(query) {
+        if (this.invert) {
+            if (query.excluded_content_type && query.excluded_content_type.startsWith(this.content_type))
+                return query;
+            return Eknc.QueryObject.new_from_object(query, {
+                excluded_content_type: this.content_type,
+            });
+        } else {
+            if (query.content_type && query.content_type.startsWith(this.content_type))
+                return query;
+            return Eknc.QueryObject.new_from_object(query, {
+                content_type: this.content_type,
+            });
+        }
+    },
+});
