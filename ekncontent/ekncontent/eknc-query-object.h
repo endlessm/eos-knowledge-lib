f94eeb8ff46fc8f22d8ea38379fce580c678246f
Martin Abente Lahaye <martin@endlessm.com>
2018-03-02 06:24:22 -0300

    ekncontent: Support sorting by title
    Until now,  alphabetical sorting of articles was done only  on
    the front-end. When mixed with with pagination, it causes some
    unexpected and confusing behaviors, like:
    
    1) Ask for the first 10 (random) articles.
    2) Sort these articles alphabetically in the front-end.
    3) Ask for next 10 (random) articles.
    4) (repeat) The order seems inconsistent each time.
    
    Solve this problem by supporting sorting by title.
    
    https://phabricator.endlessm.com/T21276

--- ekncontent/ekncontent/eknc-query-object.h
+++ ekncontent/ekncontent/eknc-query-object.h
@@ -44,6 +44,7 @@ typedef enum {
  * @EKNC_QUERY_OBJECT_SORT_SEQUENCE_NUMBER: Uses the article page rank to sort
  *   results.
  * @EKNC_QUERY_OBJECT_SORT_DATE: Uses the article date to sort the results.
+ * @EKNC_QUERY_OBJECT_SORT_ALPHABETICAL: Uses the article title to sort the results.
  *
  * Enumeration for different ways to sort query results.
  */
@@ -51,6 +52,7 @@ typedef enum {
   EKNC_QUERY_OBJECT_SORT_RELEVANCE,
   EKNC_QUERY_OBJECT_SORT_SEQUENCE_NUMBER,
   EKNC_QUERY_OBJECT_SORT_DATE,
+  EKNC_QUERY_OBJECT_SORT_ALPHABETICAL,
 } EkncQueryObjectSort;
 
 /**
