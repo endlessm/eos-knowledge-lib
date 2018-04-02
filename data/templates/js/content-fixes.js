6edf53c02d3dca305e384a49da739874f162ad17
Sam Spilsbury <sam@endlessm.com>
2018-04-02 23:53:35 +0700

    templates: Remove template files for legacy content renderer

--- data/templates/js/content-fixes.js
+++
@@ -1,6 +0,0 @@
-var citationRequiredElements = document.querySelectorAll("sup > i > a");
-for (var i = 0; i < citationRequiredElements.length; i++)
-{
-	if (citationRequiredElements[i].parentNode.parentNode.innerHTML[0] === "[")
-		citationRequiredElements[i].parentNode.parentNode.style.display = "none";
-}
