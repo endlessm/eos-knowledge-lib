675e4f1d4605142cc3d471d92c26f7b456e33df4
Dylan McCall <dylan@endlessm.com>
2019-04-04 19:20:47 -0700

    Add tests for save and print button visiblity
    https://phabricator.endlessm.com/T25706

--- tests/js/framework/widgets/testPDFView.js
+++ tests/js/framework/widgets/testPDFView.js
@@ -1,4 +1,4 @@
-const { EvinceDocument, EvinceView, Gio, Gtk } = imports.gi;
+const { DModel, EvinceDocument, EvinceView, Gio, Gtk } = imports.gi;
 
 const Utils = imports.tests.utils;
 Utils.register_gresource();
@@ -13,18 +13,60 @@ Gtk.init(null);
 EvinceDocument.init();
 
 describe('PDF view', function () {
-    let view;
-
     beforeEach(function () {
         jasmine.addMatchers(CssClassMatcher.customMatchers);
         jasmine.addMatchers(WidgetDescendantMatcher.customMatchers);
-
-        view = new PDFView.PDFView();
     });
 
-    it('opens pdfs in Evince', function () {
-        let stream = Gio.File.new_for_path(TEST_CONTENT_DIR + 'pdf-sample1.pdf').read(null);
-        view.load_stream(stream, 'application/pdf');
-        expect(view).toHaveDescendantWithClass(EvinceView.View);
+    describe('with pdf model ', function () {
+        let pdf_view;
+        beforeEach(function () {
+            pdf_model = DModel.Article.new_from_props({
+                content_type: 'application/pdf',
+                title: 'Pdf title',
+                can_export: true,
+                can_print: true,
+            });
+            pdf_model.get_content_stream = () => {
+                let file = Gio.File.new_for_path(TEST_CONTENT_DIR + 'pdf-sample1.pdf');
+                return file.read(null);
+            };
+            pdf_view = new PDFView.PDFView({
+                model: pdf_model
+            });
+        });
+
+        it('opens pdfs in Evince', function () {
+            expect(pdf_view).toHaveDescendantWithClass(EvinceView.View);
+        });
+
+        it('has save and print buttons visible', function () {
+            expect(pdf_view._save.visible).toBe(true);
+            expect(pdf_view._print.visible).toBe(true);
+        });
+    });
+
+    describe('with export and print disabled', function () {
+        let pdf_view;
+        beforeEach(function () {
+            pdf_model = DModel.Article.new_from_props({
+                content_type: 'application/pdf',
+                title: 'Pdf title',
+                can_export: false,
+                can_print: false,
+            });
+            pdf_model.get_content_stream = () => {
+                let file = Gio.File.new_for_path(TEST_CONTENT_DIR + 'pdf-sample1.pdf');
+                return file.read(null);
+            };
+            pdf_view = new PDFView.PDFView({
+                model: pdf_model
+            });
+        });
+
+        it('has save and print buttons hidden', function () {
+            expect(pdf_view._save.visible).toBe(false);
+            expect(pdf_view._print.visible).toBe(false);
+        });
     });
 });
